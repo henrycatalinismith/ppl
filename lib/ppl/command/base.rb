8f0ed6bb6cb1fb23572b5db35c8ae655bf4b2a6a
Henry Catalini Smith <henry@catalinismith.se>
2012-11-10 23:46:10 +0000

    Move code for loading all vcards out of "list" command into base for DRY

--- lib/ppl/command/base.rb
+++ lib/ppl/command/base.rb
@@ -13,6 +13,19 @@ class Ppl::Command::Base
 
   private
 
+  def all_vcards
+    pattern   = @options[:path] + "/*.vcard"
+    filenames = Dir.glob pattern
+    vcards    = {}
+
+    filenames.each do |filename|
+      id = File.basename(filename).slice(0..-7)
+      vcard = load_vcard id
+      vcards[id] = vcard
+    end
+    vcards
+  end
+
   def load_vcard(id)
     filename = @options[:path] + "/" + id + ".vcard"
     vcard    = IO.read filename
