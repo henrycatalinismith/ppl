f479a60665860933ec832d39276366a5e1c4acb5
Henry Catalini Smith <henry@catalinismith.se>
2012-11-11 09:56:22 +0000

    Add "ppl birthdays" command
    Again this isn't anywhere near complete in terms of how it'd ideally behave.
    It'd be best if it sorted people in ascending order of days until next birthday
    for one thing. And the output format could do with a bit of tweaking too.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 
+gem 'active_support'
+
 gem "colored", "~> 1.2"
 
 gem "vpim", "12.1.12", :git => "git://github.com/sam-github/vpim.git"
---
+++ lib/ppl/command/birthdays.rb
@@ -0,0 +1,44 @@
+
+require "active_support/core_ext/time/calculations.rb"
+require "active_support/core_ext/date_time/calculations.rb"
+
+class Ppl::Command::Birthdays < Ppl::Command::Base
+
+  def name
+    "birthdays"
+  end
+
+  def summary
+    "Show a list of upcoming birthdays"
+  end
+
+  def index
+    vcards = all_vcards
+    today = Date.today
+
+    vcards.each do |id, vcard|
+      if vcard.birthday.nil?
+        next
+      end
+
+      birthday = vcard.birthday.dup
+
+      while birthday < today
+        birthday = birthday.advance :years => 1
+      end
+
+      days = birthday - today
+
+      puts(
+        sprintf("%-20s", id)                  +
+        sprintf("%-20s", vcard.name.fullname) +
+        sprintf("%-20s", vcard.birthday)      +
+        sprintf("%d days", days)
+      )
+
+    end
+
+  end
+
+end
+
