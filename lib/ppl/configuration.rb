1d5940eec27e18e651d10d828ade546b924a2298
Henry Catalini Smith <henry@catalinismith.com>
2012-11-14 23:02:04 +0000

    Finally removed the hardcoded address book path that was preventing this from working on any machine but my own

---
+++ lib/ppl/configuration.rb
@@ -0,0 +1,35 @@
+
+require "inifile"
+require "deep_merge"
+
+class Ppl::Configuration
+
+  @@config_path = "~/.pplconfig"
+
+  @@defaults = {
+    "address_book" => {
+      "path" => "~/ppl"
+    }
+  }
+
+  def to_h
+
+    config_path = File.expand_path @@config_path
+
+    if File.exists? config_path
+      custom = IniFile::load(config_path)
+    end
+
+    config = @@defaults.deep_merge! custom
+
+    config["address_book"]["path"] = File.expand_path config["address_book"]["path"]
+
+    config
+  end
+
+  def [](key)
+    to_h[key]
+  end
+
+end
+
