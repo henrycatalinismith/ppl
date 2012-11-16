60007bc856c8bd97c4471fd4e350f3d9e0234e50
Henry Smith <henry@henrysmith.org>
2012-11-16 19:49:21 +0000

    Add contact:mv command

---
+++ lib/ppl/command/contact_mv.rb
@@ -0,0 +1,44 @@
+
+class Ppl::Command::Move < Ppl::Command
+
+  def name
+    "contact:mv"
+  end
+
+  def summary
+    "Rename a contact"
+  end
+
+  def banner
+    "Usage: ppl contact:mv <contact> <new name>"
+  end
+
+  def options(parser)
+  end
+
+  def commit_on_success
+    true
+  end
+
+  def execute(argv, options)
+
+    contact_id = argv.shift
+    new_id     = argv.shift
+
+    if contact_id.nil? || new_id.nil?
+      $stderr.puts @option_parser
+      return false
+    end
+
+    contact = @address_book.contact contact_id
+    if contact.nil?
+      raise "contact '#{contact_id}' not found"
+    end
+
+    @address_book.rename_contact(contact, new_id)
+
+    true
+  end
+
+end
+
