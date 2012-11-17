cf41dc229404a3d56e4eaea17d420217e7d26d43
Henry Catalini Smith <henry@catalinismith.se>
2012-11-17 15:43:41 +0000

    Allow for multiple address books per user according to config

--- lib/ppl/configuration.rb
+++ lib/ppl/configuration.rb
@@ -6,33 +6,59 @@ class Ppl::Configuration
 
   @@config_path = "~/.pplconfig"
 
-  @@defaults = {
-    "address_book" => {
-      "path" => "~/ppl"
-    },
-    "alias" => {},
-    "color" => {
-      "contact:list" => false,
-    },
-  }
-
   def to_h
 
-    config_path = File.expand_path @@config_path
-
-    if File.exists? config_path
-      custom = IniFile::load(config_path)
-    end
-
-    config = @@defaults.deep_merge! custom
+    config = {}
+    config = config.deep_merge! self.default_config
+    config = config.deep_merge! self.system_config
+    config = config.deep_merge! self.user_config
+    config = config.deep_merge! self.local_config
 
     config["address_book"]["path"] = File.expand_path config["address_book"]["path"]
-
     config
   end
 
   def [](key)
     to_h[key]
+  end
+
+  def default_config
+    {
+      "address_book" => {
+        "path" => Dir.pwd,
+      },
+      "alias" => {},
+      "color" => {
+        "contact:list" => false,
+      },
+    }
+  end
+
+  def system_config
+    {}
+  end
+
+  def user_config
+    path = File.expand_path "~/.pplconfig"
+    if File.exists? path
+      IniFile::load(path).to_h
+    end
+  end
+
+  def local_config
+    config = {}
+    path   = File.join(Dir.pwd, ".ppl")
+
+    if File.exists? path
+      cpath = File.join(path, "config")
+      if File.exists? cpath
+        config = IniFile::load(cpath).to_h
+      end
+
+      config["address_book"]["path"] = Dir.pwd
+    end
+
+    return config
   end
 
 end
