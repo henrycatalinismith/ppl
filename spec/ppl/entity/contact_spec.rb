708666da92d30cb4e41f99904a341f8041b4a959
Henry Catalini Smith <henry@catalinismith.se>
2012-11-19 08:08:15 +0000

    Another couple of contact properties

--- spec/ppl/entity/contact_spec.rb
+++ spec/ppl/entity/contact_spec.rb
@@ -1,38 +1,46 @@
 
 describe Ppl::Entity::Contact, "#email_addresses" do
-
   before(:each) do
     @contact = Ppl::Entity::Contact.new
   end
-
   it "is an array" do
       @contact.email_addresses.should be_an(Array)
   end
-
 end
 
-describe Ppl::Entity::Contact, "#postal_addresses" do
-
+describe Ppl::Entity::Contact, "#nicknames" do
   before(:each) do
     @contact = Ppl::Entity::Contact.new
   end
-
   it "is an array" do
-      @contact.postal_addresses.should be_an(Array)
+      @contact.nicknames.should be_an(Array)
   end
+end
 
+describe Ppl::Entity::Contact, "#organizations" do
+  before(:each) do
+    @contact = Ppl::Entity::Contact.new
+  end
+  it "is an array" do
+      @contact.organizations.should be_an(Array)
+  end
 end
 
 describe Ppl::Entity::Contact, "#phone_numbers" do
-
   before(:each) do
     @contact = Ppl::Entity::Contact.new
   end
-
   it "is an array" do
       @contact.phone_numbers.should be_an(Array)
   end
-
 end
 
+describe Ppl::Entity::Contact, "#postal_addresses" do
+  before(:each) do
+    @contact = Ppl::Entity::Contact.new
+  end
+  it "is an array" do
+      @contact.postal_addresses.should be_an(Array)
+  end
+end
 
