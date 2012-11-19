8c0469fb1239497fc7490282b1e201974695e2b4
Henry Catalini Smith <henry@catalinismith.se>
2012-11-19 13:46:09 +0000

    Vcard name encoding

---
+++ lib/ppl/entity/name.rb
@@ -0,0 +1,12 @@
+
+class Ppl::Entity::Name
+
+  attr_accessor :full
+  attr_accessor :family
+  attr_accessor :given
+  attr_accessor :additional
+  attr_accessor :prefix
+  attr_accessor :suffix
+
+end
+
