ab5d9ac074536a3cd8ca16f88877183aea42fb2c
Henry Catalini Smith <henry@catalinismith.com>
2012-11-19 19:25:10 +0000

    Add command selector

--- lib/ppl/application/command_selector.rb
+++ lib/ppl/application/command_selector.rb
@@ -1,12 +1,19 @@
 
 class Ppl::Application::CommandSelector
 
-  attr_accessor :commands
-  attr_accessor :aliases
+  attr_reader :commands
+  attr_reader :aliases
 
   def initialize
     @aliases  = []
     @commands = []
+  end
+
+  def add_alias(a)
+    @aliases.push a
+  end
+
+  def add_command(command)
   end
 
 end
---
+++ lib/ppl/entity/alias.rb
@@ -0,0 +1,13 @@
+
+class Ppl::Entity::Alias
+
+  attr_accessor :name
+  attr_accessor :value
+
+  def initialize(name, value)
+    @name  = name
+    @value = value
+  end
+
+end
+
