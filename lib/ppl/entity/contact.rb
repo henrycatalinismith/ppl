4402a8873dc94620c2b2a3d27818784d48fd6b62
Henry Catalini Smith <henry@catalinismith.com>
2012-11-19 08:08:15 +0000

    Another couple of contact properties

--- lib/ppl/entity/contact.rb
+++ lib/ppl/entity/contact.rb
@@ -2,13 +2,17 @@
 class Ppl::Entity::Contact
 
   attr_accessor :email_addresses
-  attr_accessor :postal_addresses
+  attr_accessor :nicknames
+  attr_accessor :organizations
   attr_accessor :phone_numbers
+  attr_accessor :postal_addresses
 
   def initialize
     @email_addresses  = []
-    @postal_addresses = []
+    @nicknames        = []
+    @organizations    = []
     @phone_numbers    = []
+    @postal_addresses = []
   end
 
 end
