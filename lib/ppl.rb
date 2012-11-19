13bd6bc559278182f9668237388756f61ca42305
Henry Catalini Smith <henry@catalinismith.se>
2012-11-19 19:44:39 +0000

    Remove command selector class. Crap idea.

--- lib/ppl.rb
+++ lib/ppl.rb
@@ -20,8 +20,6 @@ require "ppl/command"
 require "ppl/adapter/vcard"
 require "ppl/adapter/vcard/vpim"
 
-require "ppl/application/command_selector"
-
 require "ppl/entity/alias"
 require "ppl/entity/contact"
 require "ppl/entity/name"
