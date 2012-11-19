f97b5f7933990cad93df25a7fc3cbe2d8e2abe27
Henry Catalini Smith <henry@catalinismith.se>
2012-11-19 08:05:13 +0000

    Add a couple more contact properties

--- lib/ppl/entity/contact.rb
+++ lib/ppl/entity/contact.rb
@@ -2,9 +2,13 @@
 class Ppl::Entity::Contact
 
   attr_accessor :email_addresses
+  attr_accessor :postal_addresses
+  attr_accessor :phone_numbers
 
   def initialize
-    @email_addresses = []
+    @email_addresses  = []
+    @postal_addresses = []
+    @phone_numbers    = []
   end
 
 end
--- spec/ppl/entity/contact_spec.rb
+++ spec/ppl/entity/contact_spec.rb
@@ -11,3 +11,28 @@ describe Ppl::Entity::Contact, "#email_addresses" do
 
 end
 
+describe Ppl::Entity::Contact, "#postal_addresses" do
+
+  before(:each) do
+    @contact = Ppl::Entity::Contact.new
+  end
+
+  it "is an array" do
+      @contact.postal_addresses.should be_an(Array)
+  end
+
+end
+
+describe Ppl::Entity::Contact, "#phone_numbers" do
+
+  before(:each) do
+    @contact = Ppl::Entity::Contact.new
+  end
+
+  it "is an array" do
+      @contact.phone_numbers.should be_an(Array)
+  end
+
+end
+
+
