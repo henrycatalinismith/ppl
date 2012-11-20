6d213c86aa7ce34e1d104f6e4e78f43d985c1a29
Henry Catalini Smith <henry@catalinismith.se>
2012-11-20 08:56:38 +0000

    Why, yes, Henry, you *can* test that class. Stop being lazy.

--- spec/ppl/application/bootstrap_spec.rb
+++ spec/ppl/application/bootstrap_spec.rb
@@ -5,13 +5,28 @@ describe Ppl::Application::Bootstrap do
     @bootstrap = Ppl::Application::Bootstrap.new
   end
 
+  describe "#command_suite" do
+    it "should return a Ppl::Application::CommandSuite" do
+      @bootstrap.command_suite.should be_a(Ppl::Application::CommandSuite)
+    end
+    it "should contain the 'ls' command" do
+      @bootstrap.command_suite.find_command("ls").should_not be nil
+    end
+  end
+
   describe "#input" do
-    it "should return an instanceof Ppl::Application::Input" do
+    it "should return a Ppl::Application::Input" do
       @bootstrap.input.should be_a(Ppl::Application::Input)
     end
     it "should populate the object with the given arguments" do
       arguments = ["foo", "bar"]
       @bootstrap.input(arguments).arguments.should be(arguments)
+    end
+  end
+
+  describe "#shell" do
+    it "should return a Ppl::Application::Shell" do
+      @bootstrap.shell.should be_a(Ppl::Application::Shell)
     end
   end
 
