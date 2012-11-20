adb7843b648dc70ca3eff62e59e415b31219d6a7
Henry Catalini Smith <henry@catalinismith.se>
2012-11-20 18:34:42 +0000

    Add Ppl::Adapter::Storage::Disk

---
+++ spec/ppl/adapter/storage/disk_spec.rb
@@ -0,0 +1,26 @@
+
+describe Ppl::Adapter::Storage::Disk, "#initialize" do
+
+  it "should accept a path" do
+    @storage = Ppl::Adapter::Storage::Disk.new("/tmp")
+    @storage.path.should eq "/tmp"
+  end
+
+end
+
+describe Ppl::Adapter::Storage::Disk do
+
+  before(:each) do
+    @storage = Ppl::Adapter::Storage::Disk.new(nil)
+  end
+
+  describe "#vcard_adapter=" do
+    it "should accept a vcard adapter" do
+      adapter = double(Ppl::Adapter::Vcard)
+      @storage.vcard_adapter = adapter
+      @storage.vcard_adapter.should be adapter
+    end
+  end
+
+end
+
