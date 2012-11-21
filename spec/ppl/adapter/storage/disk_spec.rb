24f31635903531626c7e9db3e0a5802318f0fa12
Henry Catalini Smith <henry@catalinismith.se>
2012-11-21 08:03:23 +0000

    Filesystem interaction testing

--- spec/ppl/adapter/storage/disk_spec.rb
+++ spec/ppl/adapter/storage/disk_spec.rb
@@ -24,3 +24,23 @@ describe Ppl::Adapter::Storage::Disk do
 
 end
 
+describe Ppl::Adapter::Storage::Disk do
+
+  before(:each) do
+    @storage = Ppl::Adapter::Storage::Disk.new("/contacts")
+    @adapter = double(Ppl::Adapter::Vcard)
+    @storage.vcard_adapter = @adapter
+
+    FakeFS.activate!
+    Dir.mkdir "/contacts"
+  end
+
+  after(:each) do
+    FakeFS.deactivate!
+  end
+
+  it "is cool to swear" do
+  end
+
+end
+
