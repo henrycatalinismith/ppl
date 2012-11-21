ae89be5d6f7a0abb56ee21b07e352d3699ff3b23
Henry Catalini Smith <henry@catalinismith.com>
2012-11-21 13:09:11 +0000

    Just a tiny bit of non-TDD code to ensure this new stuff works in real life

--- lib/ppl/command/contact_list.rb
+++ lib/ppl/command/contact_list.rb
@@ -7,6 +7,12 @@ class Ppl::Command::ContactList < Ppl::Application::Command
   end
 
   def execute(input, output)
+    address_book = @storage.load_address_book
+
+    address_book.each do |contact|
+      puts contact
+    end
+
     return true
   end
 
