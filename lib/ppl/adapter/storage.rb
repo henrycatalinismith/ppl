45ac838a8fbf5553b30d2b2af414c78e86dcf7be
Henry Catalini Smith <henry@catalinismith.com>
2012-11-22 12:55:35 +0000

    Finish implementing Ppl::Command::ContactDelete

--- lib/ppl/adapter/storage.rb
+++ lib/ppl/adapter/storage.rb
@@ -1,6 +1,10 @@
 
 class Ppl::Adapter::Storage
 
+  def delete_contact(contact)
+    raise NotImplementedError
+  end
+
   def load_address_book
     raise NotImplementedError
   end
--- spec/ppl/command/contact_delete_spec.rb
+++ spec/ppl/command/contact_delete_spec.rb
@@ -4,6 +4,7 @@ describe Ppl::Command::ContactDelete do
   before(:each) do
     @input   = Ppl::Application::Input.new
     @output  = Ppl::Application::Output.new(nil, nil)
+    @contact = Ppl::Entity::Contact.new
     @command = Ppl::Command::ContactDelete.new
     @storage = double(Ppl::Adapter::Storage)
 
@@ -16,14 +17,20 @@ describe Ppl::Command::ContactDelete do
     end
   end
 
-  describe "#name" do
+  describe "#execute" do
+
     it "should throw a Ppl::Error::ContactNotFound for invalid contacts" do
       @storage.should_receive(:load_contact).and_return(nil)
-
       @input.arguments.push("nobody")
-
       expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::ContactNotFound)
     end
+
+    it "should delete the given contact" do
+      @storage.should_receive(:load_contact).and_return(@contact)
+      @storage.should_receive(:delete_contact).with(@contact)
+      @command.execute(@input, @output)
+    end
+
   end
 
 end
