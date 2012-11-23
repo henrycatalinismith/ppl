67d3a9f04b7956f041ff84d53065fab36640b0c3
Henry Catalini Smith <henry@catalinismith.se>
2012-11-23 13:22:25 +0000

    Make the show command print the contact's full name

--- spec/ppl/command/contact_show_spec.rb
+++ spec/ppl/command/contact_show_spec.rb
@@ -3,11 +3,31 @@ describe Ppl::Command::ContactShow do
 
   before(:each) do
     @command = Ppl::Command::ContactShow.new
+    @input   = Ppl::Application::Input.new
+    @output  = double(Ppl::Application::Output)
+    @contact = Ppl::Entity::Contact.new
+    @name    = Ppl::Entity::Name.new
+    @storage = double(Ppl::Adapter::Storage)
+
+    @command.storage = @storage
+    @contact.name    = @name
   end
 
   describe "#name" do
     it "should be 'show'" do
       @command.name.should eq "show"
+    end
+  end
+
+  describe "#execute" do
+    it "should show the contact's name" do
+      @storage.should_receive(:require_contact).and_return(@contact)
+
+      @contact.name.full = "John Doe"
+
+      @output.should_receive(:line).with("John Doe")
+
+      @command.execute(@input, @output)
     end
   end
 
