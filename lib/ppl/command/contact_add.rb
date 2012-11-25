f0766f7f86e74477b6f5d88d2e088e98e22f7595
Henry Smith <henry@henrysmith.org>
2012-11-25 14:15:06 +0000

    Require an ID and a name when creating a new contact

--- lib/ppl/command/contact_add.rb
+++ lib/ppl/command/contact_add.rb
@@ -7,6 +7,13 @@ class Ppl::Command::ContactAdd < Ppl::Application::Command
   end
 
   def execute(input, output)
+    contact_id = input.arguments.shift
+    full_name  = input.arguments.shift
+
+    if contact_id.nil? || full_name.nil?
+      raise Ppl::Error::IncorrectUsage
+    end
+
     return true
   end
 
