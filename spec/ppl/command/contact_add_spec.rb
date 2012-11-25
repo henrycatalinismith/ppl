d43e930274494176de7ba5609878a38a856ea304
Henry Catalini Smith <henry@catalinismith.se>
2012-11-25 14:20:01 +0000

    Make Ppl::Command::ContactAdd actually save a contact

--- spec/ppl/command/contact_add_spec.rb
+++ spec/ppl/command/contact_add_spec.rb
@@ -4,6 +4,9 @@ describe Ppl::Command::ContactAdd do
   before(:each) do
     @command = Ppl::Command::ContactAdd.new
     @input   = Ppl::Application::Input.new
+    @storage = double(Ppl::Adapter::Storage)
+
+    @command.storage = @storage
   end
 
   describe "#name" do
@@ -13,13 +16,25 @@ describe Ppl::Command::ContactAdd do
   end
 
   describe "#execute" do
+
     it "should raise a Ppl::Error::IncorrectUsage if no id is given" do
       expect{@command.execute(@input, nil)}.to raise_error(Ppl::Error::IncorrectUsage)
     end
+
     it "should raise a Ppl::Error::IncorrectUsage if no name is given" do
       @input.arguments = ["some_id"]
       expect{@command.execute(@input, nil)}.to raise_error(Ppl::Error::IncorrectUsage)
     end
+
+    it "should save a new contact" do
+      @storage.should_receive(:save_contact) do |contact|
+        contact.id.should eq "john"
+        contact.name.should eq "John Doe"
+      end
+      @input.arguments = ["john", "John Doe"]
+      @command.execute(@input, nil)
+    end
+
   end
 
 end
