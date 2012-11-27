72514c2bab0b47fbbacee89ffcc3f953a481e84e
Henry Smith <henry@henrysmith.org>
2012-11-27 18:59:24 +0000

    Add birthday to information about contact displayed by "ppl show" command

--- lib/ppl/format/contact/full.rb
+++ lib/ppl/format/contact/full.rb
@@ -9,6 +9,12 @@ class Ppl::Format::Contact::Full < Ppl::Format::Contact
       lines.push(first_line)
     end
 
+    vitals = vitals(contact)
+    if vitals != ""
+      lines.push("")
+      lines.push(vitals)
+    end
+
     return lines.join("\n")
   end
 
@@ -16,16 +22,22 @@ class Ppl::Format::Contact::Full < Ppl::Format::Contact
 
   def first_line(contact)
     line = ""
-
     if !contact.name.nil?
       line += contact.name
     end
-
     if !contact.email_address.nil?
       line += " <#{contact.email_address}>"
     end
-
     return line
+  end
+
+  def vitals(contact)
+    vitals = []
+    if !contact.birthday.nil?
+      line = sprintf("  %-12s %s", "Birthday", contact.birthday.strftime("%Y-%m-%d"))
+      vitals.push(line)
+    end
+    return vitals.join("\n")
   end
 
 end
