6e7fcfe5f11edf7008d4ecf33b444e75c8e49c69
Henry Catalini Smith <henry@catalinismith.se>
2012-11-28 14:09:47 +0000

    Better error handling for "ppl email"

--- lib/ppl/command/set_email.rb
+++ lib/ppl/command/set_email.rb
@@ -11,6 +11,14 @@ class Ppl::Command::SetEmail < Ppl::Application::Command
     contact_id    = input.arguments.shift
     email_address = input.arguments.shift
 
+    if contact_id.nil?
+      raise Ppl::Error::IncorrectUsage, "No contact specified"
+    end
+
+    if email_address.nil?
+      raise Ppl::Error::IncorrectUsage, "No email address specified"
+    end
+
     contact = @storage.require_contact(contact_id)
     contact.email_address = email_address
 
