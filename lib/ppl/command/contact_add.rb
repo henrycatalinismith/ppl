fbeb7fac268215e291d3917a8d87a4d6013b198a
Henry Smith <henry@henrysmith.org>
2012-11-28 14:03:53 +0000

    Better error handling for "ppl add"

--- lib/ppl/command/contact_add.rb
+++ lib/ppl/command/contact_add.rb
@@ -6,17 +6,25 @@ class Ppl::Command::ContactAdd < Ppl::Application::Command
     @description = "Add a new contact"
   end
 
+  def options(parser, options)
+    parser.banner = "usage: ppl add <contact> <name>"
+  end
+
   def execute(input, output)
     contact_id   = input.arguments.shift
     contact_name = input.arguments.shift
 
-    if contact_id.nil? || contact_name.nil?
-      raise Ppl::Error::IncorrectUsage
+    if contact_id.nil?
+      raise Ppl::Error::IncorrectUsage, "No contact specified"
+    end
+
+    if contact_name.nil?
+      raise Ppl::Error::IncorrectUsage, "No name specified"
     end
 
     contact = Ppl::Entity::Contact.new
-    contact.id = contact_id
-    contact.name = contact_name
+    contact.id = contact_id.dup
+    contact.name = contact_name.dup
 
     @storage.save_contact(contact)
     return true
