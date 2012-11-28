1b401beaa3d158abceedb95889708fc90ca27ab0
Henry Smith <henry@henrysmith.org>
2012-11-28 22:08:37 +0000

    Implement Ppl::Adapter::Storage::Factory#load_adapter

--- lib/ppl/adapter/storage/factory.rb
+++ lib/ppl/adapter/storage/factory.rb
@@ -2,6 +2,17 @@
 class Ppl::Adapter::Storage::Factory
 
   def load_adapter(directory)
+    git_dir = File.join(directory.path, ".git")
+
+    disk_adapter = Ppl::Adapter::Storage::Disk.new(directory)
+    adapter      = disk_adapter
+
+    if File.exists?(git_dir)
+      git_adapter = Ppl::Adapter::Storage::Git.new(disk_adapter)
+      adapter     = git_adapter
+    end
+
+    return adapter
   end
 
 end
