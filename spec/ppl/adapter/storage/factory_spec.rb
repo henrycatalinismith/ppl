b30e1a59327233bf90dec4f56206213ac22231d5
Henry Catalini Smith <henry@catalinismith.se>
2012-11-28 22:08:37 +0000

    Implement Ppl::Adapter::Storage::Factory#load_adapter

--- spec/ppl/adapter/storage/factory_spec.rb
+++ spec/ppl/adapter/storage/factory_spec.rb
@@ -2,15 +2,30 @@
 describe Ppl::Adapter::Storage::Factory do
 
   before(:each) do
-    @factory = Ppl::Adapter::Storage::Factory.new
+    FakeFS.activate!
+    Dir.mkdir "/contacts"
+
+    @factory   = Ppl::Adapter::Storage::Factory.new
+    @directory = Dir.new("/contacts")
+    @repo      = double(Rugged::Repository)
+
+    Rugged::Repository.stub(:new).and_return(@repo)
+  end
+
+  after(:each) do
+    FileUtils.rm_rf "/contacts"
+    FakeFS.deactivate!
   end
 
   describe "#load_adapter" do
 
     it "should return a disk adapter by default" do
+      @factory.load_adapter(@directory).should be_a(Ppl::Adapter::Storage::Disk)
     end
 
     it "should return a git adapter if the directory is a git repository" do
+      Dir.mkdir "/contacts/.git"
+      @factory.load_adapter(@directory).should be_a(Ppl::Adapter::Storage::Git)
     end
 
   end
