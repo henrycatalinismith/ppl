30d5bd2e5eb440f31808ddbf1d229c21048618ed
Henry Smith <henry@henrysmith.org>
2012-11-28 20:19:57 +0000

    Mock out rugged for testing purposes

--- spec/ppl/adapter/storage/git_spec.rb
+++ spec/ppl/adapter/storage/git_spec.rb
@@ -2,17 +2,32 @@
 describe Ppl::Adapter::Storage::Git, "#initialize" do
 
   before(:each) do
+    FakeFS.activate!
+    Dir.mkdir "/contacts"
+
     @disk    = double(Ppl::Adapter::Storage::Disk)
     @contact = double(Ppl::Entity::Contact)
+    @rugged  = double(Rugged::Repository)
 
+    Rugged::Repository.stub(:new).and_return(@rugged)
+
+    @disk.stub(:directory).and_return(Dir.new("/contacts"))
     @contact.stub(:id).and_return("test")
 
     @git = Ppl::Adapter::Storage::Git.new(@disk)
   end
 
+  after(:each) do
+    FileUtils.rm_rf "/contacts"
+    FakeFS.deactivate!
+  end
+
   describe "#initialize" do
     it "should accept a disk storage adapter" do
       @git.disk.should be @disk
+    end
+    it "should instantiate rugged" do
+      @git.repository.should be @rugged
     end
   end
 
