5f80659a0c779ac89fe55e877c707b492496a04c
Henry Smith <henry@henrysmith.org>
2012-11-28 14:07:31 +0000

    Better error handling for "ppl birthday"

--- lib/ppl/command/set_birthday.rb
+++ lib/ppl/command/set_birthday.rb
@@ -6,20 +6,32 @@ class Ppl::Command::SetBirthday < Ppl::Application::Command
     @description = "Change a contact's birthday"
   end
 
+  def options(parser, options)
+    parser.banner = "usage: ppl birthday <contact> <date>"
+  end
+
   def execute(input, output)
 
     contact_id = input.arguments.shift
     birthday   = input.arguments.shift
 
-    if contact_id.nil? || birthday.nil?
-      raise Ppl::Error::IncorrectUsage
+    if contact_id.nil?
+      raise Ppl::Error::IncorrectUsage, "No contact specified"
     end
 
-    birthday = Date.parse birthday
+    if birthday.nil?
+      raise Ppl::Error::IncorrectUsage, "No date specified"
+    end
+
+    begin
+      date = Date.parse birthday
+    rescue ArgumentError
+      raise Ppl::Error::IncorrectUsage, "Invalid date '#{birthday}'"
+    end
 
     contact = @storage.require_contact(contact_id)
 
-    contact.birthday = birthday
+    contact.birthday = date
 
     storage.save_contact(contact)
 
