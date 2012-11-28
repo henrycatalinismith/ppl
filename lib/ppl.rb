b90fa24c9591d18d6a70f85b5cbbfd0c40ac9338
Henry Smith <henry@henrysmith.org>
2012-11-28 08:48:38 +0000

    Add Ppl::Command::SetPhoneNumber

--- lib/ppl.rb
+++ lib/ppl.rb
@@ -46,6 +46,7 @@ require "ppl/command/contact_rename"
 require "ppl/command/set_birthday"
 require "ppl/command/set_email"
 require "ppl/command/set_name"
+require "ppl/command/set_phone_number"
 
 require "ppl/entity/address_book"
 require "ppl/entity/contact"
