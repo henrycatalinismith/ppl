1c606640830b128db8f1af0ce5dc0a34ad57fbef
Henry Catalini Smith <henry@catalinismith.com>
2012-11-28 14:02:08 +0000

    Better error handling for "ppl name"

--- lib/ppl/command/set_name.rb
+++ lib/ppl/command/set_name.rb
@@ -6,19 +6,25 @@ class Ppl::Command::SetName < Ppl::Application::Command
     @description = "Change a contact's name"
   end
 
+  def options(parser, options)
+    parser.banner = "usage: ppl name <contact> <name>"
+  end
+
   def execute(input, output)
 
     contact_id = input.arguments.shift
     name       = input.arguments.shift
 
-    if contact_id.nil? || name.nil?
-      raise Ppl::Error::IncorrectUsage
+    if contact_id.nil?
+      raise Ppl::Error::IncorrectUsage, "No contact specified"
+    end
+
+    if name.nil?
+      raise Ppl::Error::IncorrectUsage, "No name specified"
     end
 
     contact = @storage.require_contact(contact_id)
-
     contact.name = name.dup
-
     storage.save_contact(contact)
 
     return true
