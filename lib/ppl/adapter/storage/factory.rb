7e92541d8ec175a261a2ca644cc8977f2a8c8a9d
Henry Catalini Smith <henry@catalinismith.se>
2012-11-29 17:21:12 +0000

    Make git storage adapter commit changes when saving contacts

--- lib/ppl/adapter/storage/factory.rb
+++ lib/ppl/adapter/storage/factory.rb
@@ -1,15 +1,22 @@
 
 class Ppl::Adapter::Storage::Factory
 
+  def initialize(vcard_adapter)
+    @vcard_adapter = vcard_adapter
+  end
+
   def load_adapter(directory)
     git_dir = File.join(directory.path, ".git")
 
     disk_adapter = Ppl::Adapter::Storage::Disk.new(directory)
-    adapter      = disk_adapter
+    disk_adapter.vcard_adapter = @vcard_adapter
+
+    adapter = disk_adapter
 
     if File.exists?(git_dir)
       git_adapter = Ppl::Adapter::Storage::Git.new(disk_adapter)
-      adapter     = git_adapter
+      git_adapter.vcard_adapter = @vcard_adapter
+      adapter = git_adapter
     end
 
     return adapter
