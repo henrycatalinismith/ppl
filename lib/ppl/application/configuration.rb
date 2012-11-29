20ee2ecec4a04bd63319910480a143a6a9b82989
Henry Catalini Smith <henry@catalinismith.se>
2012-11-29 18:30:08 +0000

    Let the user config a global default repository

--- lib/ppl/application/configuration.rb
+++ lib/ppl/application/configuration.rb
@@ -4,6 +4,20 @@ class Ppl::Application::Configuration
   USER_CONFIG = "~/.pplconfig"
   REPO_CONFIG = "./.ppl/config"
 
+  def address_book_path
+    default_config = default_configuration
+    user_config    = user_configuration
+
+    path = default_config["address book"]["path"]
+    if !user_config["address book"].nil? && !user_config["address book"]["path"].nil?
+      path = user_config["address book"]["path"]
+    end
+
+    return path
+  end
+
+  private
+
   def default_configuration
     {
       "address book" => {
@@ -13,9 +27,11 @@ class Ppl::Application::Configuration
   end
 
   def user_configuration
+    {}
   end
 
   def repository_configuration
+    {}
   end
 
 end
--- spec/ppl/application/configuration_spec.rb
+++ spec/ppl/application/configuration_spec.rb
@@ -1,7 +1,24 @@
 
 describe Ppl::Application::Configuration do
 
+  before(:each) do
+    @config = Ppl::Application::Configuration.new
+  end
 
+  after(:each) do
+  end
+
+  describe "#address_book_path" do
+    it "should default to the current working directory" do
+      @config.address_book_path.should eq Dir.pwd
+    end
+    it "should be configurable by the user's config file" do
+      @config.stub(:user_configuration) do
+        {"address book" => {"path" => "/contacts"}}
+      end
+      @config.address_book_path.should eq "/contacts"
+    end
+  end
 
 end
 
