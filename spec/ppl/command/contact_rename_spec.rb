b67c6051dd0645d8b717cbc5f015643606ec4c7f
Henry Smith <henry@henrysmith.org>
2012-11-29 14:06:33 +0000

    Prevent mv command from overwriting existing contacts

--- spec/ppl/command/contact_rename_spec.rb
+++ spec/ppl/command/contact_rename_spec.rb
@@ -3,7 +3,7 @@ describe Ppl::Command::ContactRename do
 
   before(:each) do
     @input   = Ppl::Application::Input.new
-    @output  = Ppl::Application::Output.new(nil, nil)
+    @output  = double(Ppl::Application::Output)
     @contact = Ppl::Entity::Contact.new
     @command = Ppl::Command::ContactRename.new
     @storage = double(Ppl::Adapter::Storage)
@@ -20,8 +20,18 @@ describe Ppl::Command::ContactRename do
 
   describe "#execute" do
 
+    it "should return false if the new id is taken" do
+      @storage.should_receive(:require_contact).with("old").and_return(@contact)
+      @storage.should_receive(:require_contact).with("new").and_return(@contact)
+      @output.should_receive(:error)
+      @input.arguments = ["old", "new"]
+
+      @command.execute(@input, @output).should eq false
+    end
+
     it "should rename the given contact" do
-      @storage.should_receive(:require_contact).and_return(@contact)
+      @storage.should_receive(:require_contact).with("old").and_return(@contact)
+      @storage.should_receive(:require_contact).with("new").and_return(nil)
 
       @storage.should_receive(:delete_contact).with(@contact) do |contact|
         contact.id.should eq "old"
