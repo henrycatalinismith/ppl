5027f287d795a17cbacf14d45b842f875f5e755e
Henry Smith <henry@henrysmith.org>
2012-12-01 10:38:15 +0000

    Better error handling for ppl rm command

--- lib/ppl/command/contact_delete.rb
+++ lib/ppl/command/contact_delete.rb
@@ -6,9 +6,18 @@ class Ppl::Command::ContactDelete < Ppl::Application::Command
     @description = "Delete a contact"
   end
 
+  def options(parser, options)
+    parser.banner = "usage: ppl rm <contact>"
+  end
+
   def execute(input, output)
     contact_id = input.arguments.first
-    contact    = @storage.require_contact(contact_id)
+
+    if contact_id.nil?
+      raise Ppl::Error::IncorrectUsage, "No contact specified"
+    end
+
+    contact = @storage.require_contact(contact_id)
     @storage.delete_contact(contact)
     return true
   end
