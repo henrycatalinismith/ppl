db497c267d94c34d8eec833cbed1de1ceaff7bdd
Henry Smith <henry@henrysmith.org>
2012-12-15 23:41:58 +0000

    Add the ability for the "org" command to list organizations

--- lib/ppl/command/org.rb
+++ lib/ppl/command/org.rb
@@ -1,12 +1,14 @@
 
 class Ppl::Command::Org < Ppl::Application::Command
 
-  attr_writer :format
+  attr_writer :show_format
+  attr_writer :list_format
 
   def initialize
     @name        = "org"
     @description = "Show or change a contact's organization"
-    @format      = Ppl::Format::Contact::Organization.new
+    @show_format = Ppl::Format::Contact::Organization.new
+    @list_format = Ppl::Format::AddressBook::Organizations.new
   end
 
   def options(parser, options)
@@ -14,33 +16,37 @@ class Ppl::Command::Org < Ppl::Application::Command
   end
 
   def execute(input, output)
-
-    contact_id   = input.arguments.shift
-    organization = input.arguments.shift
-
-    if contact_id.nil?
-      raise Ppl::Error::IncorrectUsage, "No contact specified"
-    end
-
-    contact = @storage.require_contact(contact_id)
-
-    if organization.nil?
-      show_organization(contact, output)
-    else
-      set_organization(contact, organization)
-    end
-
-    return true
+    action = determine_action(input)
+    send(action, input, output)
   end
 
   private
-  
-  def show_organization(contact, output)
-    output.line(@format.process(contact))
+
+  def determine_action(input)
+    if input.arguments[0].nil?
+      :list_organizations
+    elsif input.arguments[1].nil?
+      :show_organization
+    else
+      :set_organization
+    end
   end
 
-  def set_organization(contact, organization)
-    contact.organization = organization
+  def list_organizations(input, output)
+    address_book = @storage.load_address_book
+    org_list     = @list_format.process(address_book)
+    output.line(org_list)
+  end
+
+  def show_organization(input, output)
+    contact      = @storage.require_contact(input.arguments[0])
+    organization = @show_format.process(contact)
+    output.line(organization)
+  end
+
+  def set_organization(input, output)
+    contact = @storage.require_contact(input.arguments[0])
+    contact.organization = input.arguments[1].dup
     @storage.save_contact(contact)
   end
 
