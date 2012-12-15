5f316f52c4443f0a9eb63b0e90946db3e72b8779
Henry Catalini Smith <henry@catalinismith.com>
2012-12-15 23:33:34 +0000

    Add ability for "name" command to list names

--- spec/ppl/command/name_spec.rb
+++ spec/ppl/command/name_spec.rb
@@ -7,11 +7,15 @@ describe Ppl::Command::Name do
     @contact = Ppl::Entity::Contact.new
     @command = Ppl::Command::Name.new
 
-    @storage      = double(Ppl::Adapter::Storage)
-    @show_format  = double(Ppl::Format::Contact)
+    @address_book = Ppl::Entity::AddressBook.new
 
-    @command.storage      = @storage
-    @command.show_format  = @show_format
+    @storage     = double(Ppl::Adapter::Storage)
+    @show_format = double(Ppl::Format::Contact)
+    @list_format = double(Ppl::Format::Contact)
+
+    @command.storage     = @storage
+    @command.show_format = @show_format
+    @command.list_format = @list_format
 
     @contact.id = "jim"
   end
@@ -24,12 +28,15 @@ describe Ppl::Command::Name do
 
   describe "#execute" do
 
-    it "should raise an error if no contact ID is given" do
-      @input.arguments = [nil, "Jim Jamieson"]
-      expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
+    it "should list all contact names if no contact ID is given" do
+      @storage.should_receive(:load_address_book).and_return(@address_book)
+      @list_format.should_receive(:process).and_return("all the names")
+      @output.should_receive(:line).with("all the names")
+      @input.arguments = []
+      @command.execute(@input, @output)
     end
 
-    it "should show the contact's name if no name is given" do
+    it "should show the specified contact's name if no name is given" do
       @storage.should_receive(:require_contact).and_return(@contact)
       @show_format.should_receive(:process).and_return("John Doe")
       @output.should_receive(:line).with("John Doe")
