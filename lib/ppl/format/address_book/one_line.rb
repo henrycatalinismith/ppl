18187d4a54d72d34fc5baea13f623c45f0677309
Henry Smith <henry@henrysmith.org>
2012-12-15 15:42:59 +0000

    Delete dead code

--- lib/ppl/format/address_book/one_line.rb
+++ lib/ppl/format/address_book/one_line.rb
@@ -30,55 +30,5 @@ class Ppl::Format::AddressBook::OneLine < Ppl::Format::AddressBook
     })
   end
 
-  def precompute_column_widths(address_book)
-    widths = {
-      :id    => 0,
-      :name  => 0,
-      :email => 0,
-    }
-
-    address_book.each do |contact|
-      if !contact.id.nil? && contact.id.length > widths[:id]
-        widths[:id] = contact.id.length
-      end
-      if !contact.name.nil? && contact.name.length > widths[:name]
-        widths[:name] = contact.name.length
-      end
-      if !contact.email_address.nil? && contact.email_address.length > widths[:email]
-        widths[:email] = contact.email_address.length
-      end
-    end
-
-    return widths
-  end
-
-  def format_contact(contact, column_widths)
-    line = ""
-
-    id    = ""
-    name  = ""
-    email = ""
-
-    id = "#{contact.id}:"
-
-    line = sprintf("%-#{column_widths[:id]}s", "#{contact.id}:")
-
-    if !contact.name.nil?
-      name = contact.name
-    end
-
-    if !contact.email_address.nil?
-      email = "<#{contact.email_address}>"
-    end
-
-    line = [
-      sprintf("%-#{column_widths[:id] + 1}s", id),
-      sprintf("%-#{column_widths[:name]}s", name),
-      sprintf("%-#{column_widths[:email]}s", email),
-    ].join(" ")
-
-    return line
-  end
-
 end
 
