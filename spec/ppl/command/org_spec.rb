23e59de72bbef275037518d88a0e9ed2d622b602
Henry Smith <henry@henrysmith.org>
2012-12-15 14:46:50 +0000

    Change "org" command to also be able to show organization if none is given

--- spec/ppl/command/org_spec.rb
+++ spec/ppl/command/org_spec.rb
@@ -10,6 +10,7 @@ describe Ppl::Command::Org do
     @format  = double(Ppl::Format::Contact)
 
     @command.storage = @storage
+    @command.format  = @format
     @contact.id = "tim"
   end
 
@@ -26,12 +27,15 @@ describe Ppl::Command::Org do
       expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
     end
 
-    it "should raise an error if no organization is given" do
+    it "should show the contact's organization if no new organization is given" do
+      @storage.should_receive(:require_contact).and_return(@contact)
+      @format.should_receive(:process).and_return("Example Ltd")
+      @output.should_receive(:line).with("Example Ltd")
       @input.arguments = ["jim"]
-      expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
+      @command.execute(@input, @output)
     end
 
-    it "should change the contact's organization" do
+    it "should change the contact's organization if one is given" do
       @storage.should_receive(:require_contact).and_return(@contact)
       @storage.should_receive(:save_contact) do |contact|
         contact.organization.should eq "Example Ltd"
