10b876918f915436c76cf11f8f72ba48024d6506
Henry Catalini Smith <henry@catalinismith.com>
2012-12-15 23:23:43 +0000

    Add list formatters for all contact properties

---
+++ lib/ppl/format/address_book/organizations.rb
@@ -0,0 +1,34 @@
+
+class Ppl::Format::AddressBook::Organizations < Ppl::Format::AddressBook
+
+  attr_writer :table
+
+  def initialize
+    @table = Ppl::Format::Table.new([:id, :birthday])
+  end
+
+  def process(address_book)
+    address_book.each { |contact| add_row(contact) }
+    @table.to_s
+  end
+
+
+  private
+
+  def add_row(contact)
+    contact_id   = sprintf("%s:", contact.id)
+    organization = nil
+
+    if !contact.organization.nil?
+      organization = contact.organization
+    end
+
+    @table.add_row({
+      :id           => contact_id,
+      :organization => organization,
+    })
+  end
+
+
+end
+
