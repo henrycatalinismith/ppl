354904f434dfd0e2e64a297a84ae2e3e95ceaf9b
Henry Catalini Smith <henry@catalinismith.se>
2012-12-15 23:41:58 +0000

    Add the ability for the "org" command to list organizations

--- spec/ppl/command/org_spec.rb
+++ spec/ppl/command/org_spec.rb
@@ -7,10 +7,14 @@ describe Ppl::Command::Org do
     @output  = Ppl::Application::Output.new(nil, nil)
     @contact = Ppl::Entity::Contact.new
     @storage = double(Ppl::Adapter::Storage)
-    @format  = double(Ppl::Format::Contact)
 
-    @command.storage = @storage
-    @command.format  = @format
+    @show_format = double(Ppl::Format::Contact)
+    @list_format = double(Ppl::Format::Contact)
+
+    @command.storage     = @storage
+    @command.show_format = @show_format
+    @command.list_format = @list_format
+
     @contact.id = "tim"
   end
 
@@ -22,14 +26,17 @@ describe Ppl::Command::Org do
 
   describe "#execute" do
 
-    it "should raise an error if no contact ID is given" do
-      @input.arguments = [nil, "Example Ltd"]
-      expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
+    it "should list all organizations if no contact ID is given" do
+      @storage.should_receive(:load_address_book).and_return(@address_book)
+      @list_format.should_receive(:process).and_return("all the organizations")
+      @output.should_receive(:line).with("all the organizations")
+      @input.arguments = []
+      @command.execute(@input, @output)
     end
 
     it "should show the contact's organization if no new organization is given" do
       @storage.should_receive(:require_contact).and_return(@contact)
-      @format.should_receive(:process).and_return("Example Ltd")
+      @show_format.should_receive(:process).and_return("Example Ltd")
       @output.should_receive(:line).with("Example Ltd")
       @input.arguments = ["jim"]
       @command.execute(@input, @output)
