f173126e95bea70d7096e26da27713b0f9f66356
Henry Catalini Smith <henry@catalinismith.com>
2012-12-15 14:49:57 +0000

    Change "phone" command to also show number if none is given

--- lib/ppl/command/phone.rb
+++ lib/ppl/command/phone.rb
@@ -1,13 +1,16 @@
 
 class Ppl::Command::Phone < Ppl::Application::Command
 
+  attr_writer :format
+
   def initialize
     @name        = "phone"
-    @description = "Change a contact's phone number"
+    @description = "Show or change a contact's phone number"
+    @format      = Ppl::Format::Contact::PhoneNumber.new
   end
 
   def options(parser, options)
-    parser.banner = "usage: ppl phone <contact> <number>"
+    parser.banner = "usage: ppl phone <contact> [<number>]"
   end
 
   def execute(input, output)
@@ -19,15 +22,26 @@ class Ppl::Command::Phone < Ppl::Application::Command
       raise Ppl::Error::IncorrectUsage, "No contact specified"
     end
 
+    contact = @storage.require_contact(contact_id)
+
     if phone_number.nil?
-      raise Ppl::Error::IncorrectUsage, "No phone number specified"
+      show_phone_number(contact, output)
+    else
+      set_phone_number(contact, phone_number)
     end
 
-    contact = @storage.require_contact(contact_id)
-    contact.phone_number = phone_number
-    storage.save_contact(contact)
-
     return true
+  end
+
+  private
+
+  def show_phone_number(contact, output)
+    output.line(@format.process(contact))
+  end
+
+  def set_phone_number(contact, phone_number)
+    contact.phone_number = phone_number
+    @storage.save_contact(contact)
   end
 
 end
