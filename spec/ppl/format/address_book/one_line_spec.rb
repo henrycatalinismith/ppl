0e5f7c6d6529de8621c2fc34260a27a5dfa07058
Henry Smith <henry@henrysmith.org>
2012-12-15 15:41:46 +0000

    Remove all column alignment code from Ppl::Format::AddressBook::OneLine

--- spec/ppl/format/address_book/one_line_spec.rb
+++ spec/ppl/format/address_book/one_line_spec.rb
@@ -5,8 +5,10 @@ describe Ppl::Format::AddressBook::OneLine do
     @format       = Ppl::Format::AddressBook::OneLine.new
     @address_book = Ppl::Entity::AddressBook.new
     @contact      = Ppl::Entity::Contact.new
+    @table        = double(Ppl::Format::Table)
 
     @contact.id = "test"
+    @format.table = @table
 
     @address_book.add_contact(@contact)
   end
@@ -14,44 +16,23 @@ describe Ppl::Format::AddressBook::OneLine do
   describe "#process" do
 
     it "should at least show the contact's id" do
-      @format.process(@address_book).should include "test:"
+      @table.should_receive(:add_row).with({
+        :id    => "test:",
+        :name  => nil,
+        :email => nil,
+      })
+      @format.process(@address_book)
     end
 
-    it "should show the contact's name if available" do
+    it "should show all the info if it's available" do
       @contact.name = "John Doe"
-      @format.process(@address_book).should include "John Doe"
-    end
-
-    it "should show the contact's email address if available" do
-      @contact.email_address = "john@example.org"
-      @format.process(@address_book).should include "<john@example.org>"
-    end
-
-    it "should show their name and email if possible" do
-      @contact.name          = "John Doe"
-      @contact.email_address = "john@example.org"
-      @format.process(@address_book).should include "John Doe <john@example.org>"
-    end
-
-    it "should arrage the list into readable columns" do
-      @contact.name          = "John Doe"
-      @contact.email_address = "john@example.org"
-
-      long_contact = Ppl::Entity::Contact.new
-      long_contact.id            = "reallyreallylongid"
-      long_contact.name          = "Really Really Long Name"
-      long_contact.email_address = "reallyreallylong@example.org"
-      @address_book.add_contact(long_contact)
-
-      output = @format.process(@address_book)
-
-      [
-        "test:               John Doe                <john@example.org>",
-        "reallyreallylongid: Really Really Long Name <reallyreallylong@example.org>",
-      ].each do |line|
-        output.should include line
-      end
-
+      @contact.email_address = "jdoe@example.org"
+      @table.should_receive(:add_row).with({
+        :id    => "test:",
+        :name  => "John Doe",
+        :email => "<jdoe@example.org>",
+      })
+      @format.process(@address_book)
     end
 
   end
