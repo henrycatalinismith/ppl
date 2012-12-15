17e01004ec70020db95d6a0bd4cb36fc8471c1d1
Henry Catalini Smith <henry@catalinismith.com>
2012-12-15 15:41:46 +0000

    Remove all column alignment code from Ppl::Format::AddressBook::OneLine

--- lib/ppl/format/address_book/one_line.rb
+++ lib/ppl/format/address_book/one_line.rb
@@ -1,19 +1,34 @@
 
 class Ppl::Format::AddressBook::OneLine < Ppl::Format::AddressBook
 
+  attr_writer :table
+
+  def initialize
+    @table = Ppl::Format::Table.new([:id, :name, :email])
+  end
+
   def process(address_book)
-    lines = []
-
-    column_widths = precompute_column_widths(address_book)
-
-    address_book.each do |contact|
-      lines.push(format_contact(contact, column_widths))
-    end
-
-    return lines.join("\n")
+    address_book.each { |contact| add_row(contact) }
+    @table.to_s
   end
 
   private
+
+  def add_row(contact)
+    id    = sprintf("%s:", contact.id)
+    name  = contact.name
+    email = nil
+
+    if !contact.email_address.nil?
+      email = sprintf("<%s>", contact.email_address)
+    end
+
+    @table.add_row({
+      :id    => id,
+      :name  => name,
+      :email => email,
+    })
+  end
 
   def precompute_column_widths(address_book)
     widths = {
