c0939ab0625d7856a2d45e7405155b6c75d5be58
Henry Smith <henry@henrysmith.org>
2012-12-15 14:21:51 +0000

    Change email command to also show address if none given

--- spec/ppl/command/email_spec.rb
+++ spec/ppl/command/email_spec.rb
@@ -10,6 +10,7 @@ describe Ppl::Command::Email do
     @format  = double(Ppl::Format::Contact)
 
     @command.storage = @storage
+    @command.format  = @format
     @contact.id = "jim"
   end
 
@@ -21,23 +22,26 @@ describe Ppl::Command::Email do
 
   describe "#execute" do
 
-    it "should change the contact's email address" do
+    it "should raise an error if no contact ID is given" do
+      @input.arguments = [nil, "test@example.org"]
+      expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
+    end
+
+    it "should show the current address if no new address is given" do
+      @storage.should_receive(:require_contact).and_return(@contact)
+      @format.should_receive(:process).and_return("jdoe@example.org")
+      @output.should_receive(:line).with("jdoe@example.org")
+      @input.arguments = ["jim"]
+      @command.execute(@input, @output)
+    end
+
+    it "should change the contact's email address if an address is given" do
       @storage.should_receive(:require_contact).and_return(@contact)
       @storage.should_receive(:save_contact) do |contact|
         contact.email_address.should eq "jim@example.org"
       end
       @input.arguments = ["jim", "jim@example.org"]
       @command.execute(@input, @output)
-    end
-
-    it "should raise an error if no contact ID is given" do
-      @input.arguments = [nil, "test@example.org"]
-      expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
-    end
-
-    it "should raise an error if no email address is given" do
-      @input.arguments = ["test"]
-      expect{@command.execute(@input, @output)}.to raise_error(Ppl::Error::IncorrectUsage)
     end
 
   end
