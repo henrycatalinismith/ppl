6f775b67ffadcb5f7c0af82d9c78419fa81f63c6
Henry Catalini Smith <henry@catalinismith.com>
2012-12-20 18:32:00 +0000

    Hook up all the postal address properties to the CLI frontend

--- lib/ppl/entity/postal_address.rb
+++ lib/ppl/entity/postal_address.rb
@@ -1,7 +1,12 @@
 
 class Ppl::Entity::PostalAddress
 
+  attr_accessor :country
+  attr_accessor :locality
   attr_accessor :street
+  attr_accessor :po_box
+  attr_accessor :postal_code
+  attr_accessor :region
 
 end
 
