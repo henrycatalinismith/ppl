38d8ef06e84d95a01b17dd8859f409c03088bdaf
Henry Catalini Smith <henry@catalinismith.se>
2012-12-20 12:36:41 +0000

    Add Ppl::Format::AddressBook::MuttQuery

---
+++ spec/ppl/format/address_book/mutt_query_spec.rb
@@ -0,0 +1,30 @@
+
+describe Ppl::Format::AddressBook::MuttQuery do
+
+  before(:each) do
+    @format       = Ppl::Format::AddressBook::MuttQuery.new
+    @address_book = Ppl::Entity::AddressBook.new
+    @contact      = Ppl::Entity::Contact.new
+    @table        = double(Ppl::Format::Table)
+
+    @contact.email_address = "test@example.org"
+    @contact.name          = "Test Contact"
+
+    @format.table = @table
+    @address_book.add_contact(@contact)
+  end
+
+  describe "#process" do
+
+    it "should list the given contacts" do
+      @table.should_receive(:add_row).with({
+        :email => "test@example.org",
+        :name  => "Test Contact",
+      })
+      @format.process(@address_book)
+    end
+
+  end
+
+end
+
