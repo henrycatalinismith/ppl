76f33dad23a492eec61f64ed788ae260fc5cd2b0
Henry Catalini Smith <henry@catalinismith.se>
2012-12-23 21:44:36 +0000

    Allow command name and description to be set at class level
    This allows these always-required properties to be defined in the class body
    without the def initialize boilerplate

--- lib/ppl/application/command.rb
+++ lib/ppl/application/command.rb
@@ -1,7 +1,6 @@
 
 class Ppl::Application::Command
 
-  attr_accessor :name
   attr_accessor :description
   attr_accessor :storage
 
@@ -10,6 +9,41 @@ class Ppl::Application::Command
   end
 
   def options(parser, options)
+  end
+
+  @@names = {}
+  @@descriptions = {}
+
+  def self.name(name = nil)
+    @@names[self] = name || @@names[self]
+  end
+
+  def self.description(description = nil)
+    @@descriptions[self] = description || @@descriptions[self]
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    if @@names[self.class].nil?
+      @name
+    else
+      @@names[self.class]
+    end
+  end
+
+  def description=(description)
+    @description = description
+  end
+
+  def description
+    if @@descriptions[self.class].nil?
+      @description
+    else
+      @@descriptions[self.class]
+    end
   end
 
 end
