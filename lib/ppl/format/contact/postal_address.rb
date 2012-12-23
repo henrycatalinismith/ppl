667610460e6fc6331ec423ad103aaa8c1b9b3a75
Henry Smith <henry@henrysmith.org>
2012-12-23 00:59:26 +0000

    Fix error in "ppl post" when showing address of a contact with no address

--- lib/ppl/format/contact/postal_address.rb
+++ lib/ppl/format/contact/postal_address.rb
@@ -10,20 +10,22 @@ class Ppl::Format::Contact::PostalAddress < Ppl::Format::Contact
   def process(contact)
     address = contact.postal_address
 
-    {
-      :street      => "Street",
-      :postal_code => "Postal Code",
-      :po_box      => "PO box",
-      :locality    => "Locality",
-      :region      => "Region",
-      :country     => "Country",
-    }.each do |property, name|
-      value = address.send(property)
-      next if value.nil? || value == ""
-      @table.add_row({
-        :label => sprintf("%s:", name),
-        :value => address.send(property)
-      })
+    if !address.nil?
+      {
+        :street      => "Street",
+        :postal_code => "Postal Code",
+        :po_box      => "PO box",
+        :locality    => "Locality",
+        :region      => "Region",
+        :country     => "Country",
+      }.each do |property, name|
+        value = address.send(property)
+        next if value.nil? || value == ""
+        @table.add_row({
+          :label => sprintf("%s:", name),
+          :value => address.send(property)
+        })
+      end
     end
 
     return @table.to_s
