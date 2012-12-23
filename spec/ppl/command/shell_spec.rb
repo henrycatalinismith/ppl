49e35ed70bdf4310034ac40cf75ed318a221122e
Henry Smith <henry@henrysmith.org>
2012-12-23 18:17:56 +0000

    Refactor Ppl::Command::Shell and increase test coverage

--- spec/ppl/command/shell_spec.rb
+++ spec/ppl/command/shell_spec.rb
@@ -14,31 +14,30 @@ describe Ppl::Command::Shell do
   end
 
   describe "#execute" do
-    before(:each) do
-      @output.should_receive(:line)
-    end
     it "should read a line of input from stdin" do
       Readline.should_receive(:readline)
+      @command.stub(:terminate_gracefully)
       @command.execute(@input, @output).should eq true
     end
   end
 
   describe "#execute" do
+
     before(:each) do
-      @output.should_receive(:line)
+      @input.stdin = double(IO)
+      @input.stdin.stub(:tty?) { true }
     end
 
     it "should not show a prompt if stdin isn't a tty" do
-      @input.stdin = double(IO)
       @input.stdin.stub(:tty?) { false }
       Readline.should_receive(:readline).with("", true)
       @command.execute(@input, @output)
     end
 
     it "should show a prompt if stdin is a tty" do
-      @input.stdin = double(IO)
       @input.stdin.stub(:tty?) { true }
       Readline.should_receive(:readline).with("ppl> ", true)
+      @command.stub(:terminate_gracefully)
       @command.execute(@input, @output)
     end
 
@@ -48,12 +47,35 @@ describe Ppl::Command::Shell do
       Kernel.should_receive(:system) do |command|
         command.should include "email fred"
       end
+      @command.stub(:terminate_gracefully)
       @command.execute(@input, @output).should eq true
     end
 
     it "should exit on ctrl+c" do
       Readline.should_receive(:readline).and_raise(Interrupt)
+      @command.stub(:terminate_gracefully)
       @command.execute(@input, @output).should eq false
+    end
+
+    it "should print a final newline on EOF so the user's prompt looks nice" do
+      @input.stdin.stub(:tty?) { true }
+      @output.should_receive(:line).with("")
+      Readline.should_receive(:readline).and_return(false)
+      @command.execute(@input, @output)
+    end
+
+    it "should print a final newline on error so the user's prompt looks nice" do
+      @input.stdin.stub(:tty?) { true }
+      @output.should_receive(:line).with("")
+      @command.stub(:shell).and_raise(Interrupt)
+      @command.execute(@input, @output)
+    end
+
+    it "should not print a final newline on EOF if stdin isn't a tty" do
+      @input.stdin.stub(:tty?) { false }
+      @output.should_not_receive(:line).with("")
+      Readline.should_receive(:readline).and_return(false)
+      @command.execute(@input, @output)
     end
 
   end
