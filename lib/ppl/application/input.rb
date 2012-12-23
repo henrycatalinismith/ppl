5bdb4c33aa09b61d133e3c7976beb0a7afb8459c
Henry Catalini Smith <henry@catalinismith.se>
2012-12-23 17:45:42 +0000

    Don't show "ppl> " prompt in "ppl shell" command if stdin isn't a tty

--- lib/ppl/application/input.rb
+++ lib/ppl/application/input.rb
@@ -3,10 +3,12 @@ class Ppl::Application::Input
 
   attr_accessor :arguments
   attr_accessor :options
+  attr_accessor :stdin
 
   def initialize(arguments=[])
     @arguments = arguments
     @options   = {}
+    @stdin     = $stdin
   end
 
 end
