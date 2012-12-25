731494232e185dcdf4d224e438e994e10751381b
Henry Catalini Smith <henry@catalinismith.com>
2012-12-25 23:58:11 +0000

    Tidy up the tests for Ppl::Application::Command

--- spec/ppl/application/command_spec.rb
+++ spec/ppl/application/command_spec.rb
@@ -1,77 +1,91 @@
 
-describe Ppl::Application::Command, "#execute" do
+describe Ppl::Application::Command do
 
   before(:each) do
     @command = Ppl::Application::Command.new
   end
 
-  it "should raise not implemented error" do
-    expect{@command.execute(nil, nil)}.to raise_error(NotImplementedError)
-  end
-
-  it "should accept a storage adapter" do
-    storage = double(Ppl::Adapter::Storage)
-    @command.storage = storage
-    @command.storage.should be storage
-  end
-
-  it "should allow new properties to be defined" do
-    class TestCommand123 < Ppl::Application::Command
-      add_property :some_property
+  describe "#execute" do
+    it "should raise not implemented error" do
+      expect{@command.execute(nil, nil)}.to raise_error(NotImplementedError)
     end
   end
 
-  it "should allow properties to have values assigned to them" do
-    class TestCommand456 < Ppl::Application::Command
-      add_property :some_property
-      some_property "a value"
+  describe "#storage=" do
+    it "should accept a storage adapter" do
+      storage = double(Ppl::Adapter::Storage)
+      @command.storage = storage
+      @command.storage.should be storage
     end
-    TestCommand456.some_property.should eq "a value"
   end
 
-  it "should expose properties as instance variables too" do
-    class TestCommand789 < Ppl::Application::Command
-      add_property :some_property
-      some_property "a value"
+  describe "#add_property" do
+    it "should allow new properties to be defined" do
+      class TestCommand123 < Ppl::Application::Command
+        add_property :some_property
+      end
     end
-    instance = TestCommand789.new
-    instance.some_property.should eq "a value"
-  end
 
-  it "should allow properties to be overwritten by instance variables" do
-    class TestCommand901 < Ppl::Application::Command
-      add_property :some_property
-      some_property "a value"
+    it "should allow properties to have values assigned to them" do
+      class TestCommand456 < Ppl::Application::Command
+        add_property :some_property
+        some_property "a value"
+      end
+      TestCommand456.some_property.should eq "a value"
     end
-    instance = TestCommand901.new
-    instance.some_property = "different"
-    instance.some_property.should eq "different"
-  end
 
-  it "should allow the name to be set as an instance variable" do
-    @command.name = "testing"
-    @command.name.should eq "testing"
-  end
-
-  it "should allow the description to be set as an instance variable" do
-    @command.description = "testing"
-    @command.description.should eq "testing"
-  end
-
-  it "should allow the name to be set as a class variable" do
-    class TestCommand123 < Ppl::Application::Command
-      name "new_for_test"
+    it "should expose properties as instance variables too" do
+      class TestCommand789 < Ppl::Application::Command
+        add_property :some_property
+        some_property "a value"
+      end
+      instance = TestCommand789.new
+      instance.some_property.should eq "a value"
     end
-    command = TestCommand123.new
-    command.name.should eq "new_for_test"
+
+    it "should allow properties to be overwritten by instance variables" do
+      class TestCommand901 < Ppl::Application::Command
+        add_property :some_property
+        some_property "a value"
+      end
+      instance = TestCommand901.new
+      instance.some_property = "different"
+      instance.some_property.should eq "different"
+    end
   end
 
-  it "should allow the description to be set as a class variable" do
-    class TestCommand123 < Ppl::Application::Command
-      description "desc_for_test"
+  describe "#name=" do
+    it "should allow the name to be set as an instance variable" do
+      @command.name = "testing"
+      @command.name.should eq "testing"
     end
-    command = TestCommand123.new
-    command.description.should eq "desc_for_test"
+  end
+
+  describe "#description=" do
+    it "should allow the description to be set as an instance variable" do
+      @command.description = "testing"
+      @command.description.should eq "testing"
+    end
+  end
+
+  describe "#name" do
+    it "should allow the name to be set as a class variable" do
+      class TestCommand123 < Ppl::Application::Command
+        name "new_for_test"
+      end
+      command = TestCommand123.new
+      command.name.should eq "new_for_test"
+    end
+  end
+
+  describe "#desecription" do
+    it "should allow the description to be set as a class variable" do
+      class TestCommand123 < Ppl::Application::Command
+        description "desc_for_test"
+      end
+      command = TestCommand123.new
+      command.description.should eq "desc_for_test"
+    end
   end
 
 end
