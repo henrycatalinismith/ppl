c8c54c0de7e82ab6f258fef15f8033cb4f589d76
Henry Smith <henry@henrysmith.org>
2012-12-29 00:24:26 +0000

    Change the output format of ppl show substantially.
    In part this change is motivated by issue #14, in which @storge pointed out that
    the ppl show command only displays one of the contact's email addresses. This
    gives the impression that only one can be stored per contact, so it needed
    changing urgently.
    
    Another reason for this change was that the output of ppl show was on the whole
    quite an ugly, disorganised mess. This commit changes the rest of the formatting
    somewhat so that hopefully it'll be a little more clear.

---
+++ spec/ppl/format/postal_address_spec.rb
@@ -0,0 +1,18 @@
+
+describe Ppl::Format::PostalAddress do
+
+  before(:each) do
+    @format = Ppl::Format::PostalAddress.new
+  end
+
+  describe "#process" do
+
+    it "should raise a NotImplementedError" do
+      expect{@format.process(nil)}.to raise_error(NotImplementedError)
+    end
+
+  end
+
+end
+
+
