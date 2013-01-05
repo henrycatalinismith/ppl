8d8d75bf8cb60007c2da73d3e36d8892fb05581e
Henry Catalini Smith <henry@catalinismith.com>
2013-01-05 14:44:39 +0000

    Drastically simplify the code in Ppl::Format::Contact::Urls

--- lib/ppl/format/contact/urls.rb
+++ lib/ppl/format/contact/urls.rb
@@ -2,11 +2,7 @@
 class Ppl::Format::Contact::Urls < Ppl::Format::Contact
 
   def process(contact)
-    lines = []
-    contact.urls.each do |url|
-      lines.push url
-    end
-    lines.join("\n")
+    contact.urls.join("\n")
   end
 
 end
