a2a739809b4d358bd9e79b8c036d320eaa7aef34
Henry Catalini Smith <henry@catalinismith.se>
2013-01-07 22:02:45 +0000

    Begin work on a generic "attribute" command to reduce duplication

---
+++ lib/ppl/command/attribute.rb
@@ -0,0 +1,38 @@
+
+class Ppl::Command::Attribute < Ppl::Application::Command
+
+  attr_writer :list_format
+  attr_writer :show_format
+
+  def execute(input, output)
+    action = determine_action(input)
+    send(action, input, output)
+  end
+
+
+  private
+
+  def determine_action(input)
+    :none
+  end
+
+  def list_attribute(input, output)
+    address_book = @storage.load_address_book
+    output.line(@list_format.process(address_book))
+    true
+  end
+
+  def show_attribute(input, output)
+    contact = @storage.require_contact(input.arguments[0])
+    output.line(@show_format.process(contact))
+    true
+  end
+
+  def add_attribute(input, output)
+  end
+
+  def remove_attribute(input, output)
+  end
+
+end
+
---
+++ spec/ppl/command/attribute_spec.rb
@@ -0,0 +1,51 @@
+
+describe Ppl::Command::Attribute do
+
+  before(:each) do
+    @command = Ppl::Command::Attribute.new
+    @input = Ppl::Application::Input.new
+    @output = double(Ppl::Application::Output)
+    @storage = double(Ppl::Adapter::Storage)
+    @list_format = double(Ppl::Format::AddressBook)
+    @show_format = double(Ppl::Format::Contact)
+    @address_book = double(Ppl::Entity::AddressBook)
+    @contact = double(Ppl::Entity::Contact)
+
+    @command.list_format = @list_format
+    @command.show_format = @show_format
+    @command.storage = @storage
+  end
+
+  describe "#list_attribute" do
+
+    before(:each) do
+      @command.stub(:determine_action) { :list_attribute }
+    end
+
+    it "should list all the contacts and the value of the attribute for each" do
+      @storage.should_receive(:load_address_book).and_return(@address_book)
+      @list_format.should_receive(:process).and_return("imagine this is a list")
+      @output.should_receive(:line).with("imagine this is a list")
+      @command.execute(@input, @output).should eq true
+    end
+
+  end
+
+  describe "#show_attribute" do
+
+    before(:each) do
+      @command.stub(:determine_action) { :show_attribute }
+    end
+
+    it "should show the full list of attributes for the given contact" do
+      @input.arguments.push("jdoe")
+      @storage.should_receive(:require_contact).and_return(@contact)
+      @show_format.should_receive(:process).and_return("all the info")
+      @output.should_receive(:line).with("all the info")
+      @command.execute(@input, @output).should eq true
+    end
+
+  end
+
+end
+
