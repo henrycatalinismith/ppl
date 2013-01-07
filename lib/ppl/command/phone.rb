445c38e224922e373fa858c398311d7ef132d8f0
Henry Catalini Smith <henry@catalinismith.com>
2013-01-07 22:27:00 +0000

    Implement Ppl::Command::Attribute#add_attribute and deduplicate `ppl email`

--- lib/ppl/command/phone.rb
+++ lib/ppl/command/phone.rb
@@ -5,6 +5,7 @@ class Ppl::Command::Phone < Ppl::Command::Attribute
   description "List, show or change phone numbers"
 
   def initialize
+    @attribute   = :phone_numbers
     @show_format = Ppl::Format::Contact::PhoneNumber.new
     @list_format = Ppl::Format::AddressBook::PhoneNumbers.new
   end
