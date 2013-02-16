e804199e58a22bb9dc77f0a9d8b1abd5557ad4fd
Henry Catalini Smith <henry@catalinismith.se>
2013-02-16 10:32:31 +0000

    Add the 'age' command for displaying contacts' ages
    Incidentally, Ruby appears to lack an equivalent to PHP's DateInterval class.
    There's time_diff, but that's been broken for over a year
    (https://github.com/abhidsm/time_diff/issues/2). In the end I gave up and went
    with a code snippet from Stack Overflow, which is the sort of programming
    practice that I despise. Ruby's "optimisation for programmer happiness" isn't
    look quite so optimal right now.

---
+++ spec/ppl/format/address_book/ages_spec.rb
@@ -0,0 +1,37 @@
+
+describe Ppl::Format::AddressBook::Ages do
+
+  before(:each) do
+    @format       = Ppl::Format::AddressBook::Ages.new
+    @address_book = Ppl::Entity::AddressBook.new
+    @contact      = Ppl::Entity::Contact.new
+    @table        = double(Ppl::Format::Table)
+    @contact.id   = "test"
+    @format.table = @table
+    @format.date  = Date.parse("1980-01-02")
+    @address_book.contacts.push(@contact)
+  end
+
+  describe "#process" do
+
+    it "should at least show the contact's id" do
+      @table.should_receive(:add_row).with({
+        :id  => "test:",
+        :age => "",
+      })
+      @format.process(@address_book)
+    end
+
+    it "should show an age if it's available" do
+      @contact.birthday = Date.parse("1970-01-01")
+      @table.should_receive(:add_row).with({
+        :id  => "test:",
+        :age => "10",
+      })
+      @format.process(@address_book)
+    end
+
+  end
+
+end
+
