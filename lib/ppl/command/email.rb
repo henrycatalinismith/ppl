08f9591fa9b2988782b343d1c18308702668df49
Henry Catalini Smith <henry@catalinismith.com>
2013-02-20 07:46:25 +0000

    Inject the email command's format dependencies

--- lib/ppl/command/email.rb
+++ lib/ppl/command/email.rb
@@ -4,12 +4,6 @@ class Ppl::Command::Email < Ppl::Command::Attribute
   name        "email"
   description "Show or change a contact's email address"
 
-  def initialize
-    @attribute   = :email_addresses
-    @show_format = Ppl::Format::Contact::EmailAddresses.new
-    @list_format = Ppl::Format::AddressBook::EmailAddresses.new
-  end
-
   def options(parser, options)
     parser.banner = "usage: ppl email <contact> [<email-address>]"
     parser.on("-d", "--delete", "delete email address") do
