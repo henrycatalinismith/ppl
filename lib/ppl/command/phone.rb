644cc1201453fd917dee873f623fe49c3b98ffbc
Henry Catalini Smith <henry@catalinismith.com>
2013-02-20 08:52:17 +0000

    Inject the phone, post and url commands' formatting dependencies

--- lib/ppl/command/phone.rb
+++ lib/ppl/command/phone.rb
@@ -5,9 +5,7 @@ class Ppl::Command::Phone < Ppl::Command::Attribute
   description "List, show or change phone numbers"
 
   def initialize
-    @attribute   = :phone_numbers
-    @show_format = Ppl::Format::Contact::PhoneNumber.new
-    @list_format = Ppl::Format::AddressBook::PhoneNumbers.new
+    @attribute = :phone_numbers
   end
 
   def options(parser, options)
