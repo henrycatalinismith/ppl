9494b792d3d52a47d382e6c4c0bfa75e98a3e9df
Henry Catalini Smith <henry@catalinismith.se>
2013-02-20 08:42:30 +0000

    Reinstate some code that I shouldn't have deleted a moment ago

--- lib/ppl/command/email.rb
+++ lib/ppl/command/email.rb
@@ -4,6 +4,10 @@ class Ppl::Command::Email < Ppl::Command::Attribute
   name        "email"
   description "Show or change a contact's email address"
 
+  def initialize
+    @attribute = :email_addresses
+  end
+
   def options(parser, options)
     parser.banner = "usage: ppl email <contact> [<email-address>]"
     parser.on("-d", "--delete", "delete email address") do
