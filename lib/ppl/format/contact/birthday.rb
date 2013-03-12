5d612948f380afbdd0ae69142dd7a1595a875c5f
Henry Catalini Smith <henry@catalinismith.se>
2013-03-12 18:10:00 +0000

    Add colorization support to most stuff in Ppl::Format::Contact::*

--- lib/ppl/format/contact/birthday.rb
+++ lib/ppl/format/contact/birthday.rb
@@ -1,10 +1,21 @@
 
 class Ppl::Format::Contact::Birthday < Ppl::Format::Contact
 
+  attr_writer :color_adapter
+  attr_writer :colors
+
+  def initialize(colors={})
+    @colors = colors
+    @color_adapter = Ppl::Adapter::Color::Colored.new
+  end
+
   def process(contact)
     output = ""
     if !contact.birthday.nil?
       output += contact.birthday.strftime("%Y-%m-%d")
+    end
+    if @colors["birthday"]
+      output = @color_adapter.colorize(output, @colors["birthday"])
     end
     return output
   end
--- lib/ppl/format/contact/nicknames.rb
+++ lib/ppl/format/contact/nicknames.rb
@@ -1,8 +1,26 @@
 
 class Ppl::Format::Contact::Nicknames < Ppl::Format::Contact
 
+  attr_writer :color_adapter
+  attr_writer :colors
+
+  def initialize(colors={})
+    @colors = colors
+    @color_adapter = Ppl::Adapter::Color::Colored.new
+  end
+
   def process(contact)
-    contact.nicknames.join("\n")
+    colorize_output(contact.nicknames.join("\n"))
+  end
+
+  private
+
+  def colorize_output(string)
+    if @colors["nicknames"]
+      @color_adapter.colorize(string, @colors["nicknames"])
+    else
+      string
+    end
   end
 
 end
