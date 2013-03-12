9fa5cb6d3768df6568d5ca08114768a2de2b173b
Henry Smith <henry@henrysmith.org>
2013-03-12 18:10:00 +0000

    Add colorization support to most stuff in Ppl::Format::Contact::*

--- spec/ppl/format/contact/birthday_spec.rb
+++ spec/ppl/format/contact/birthday_spec.rb
@@ -4,6 +4,8 @@ describe Ppl::Format::Contact::Birthday do
   before(:each) do
     @format  = Ppl::Format::Contact::Birthday.new
     @contact = Ppl::Entity::Contact.new
+    @color   = double(Ppl::Adapter::Color)
+    @format.color_adapter = @color
   end
 
   describe "#process" do
@@ -15,6 +17,12 @@ describe Ppl::Format::Contact::Birthday do
     it "should return the contact's birthday if it is set" do
       @contact.birthday = Date.parse("1970-01-01")
       @format.process(@contact).should eq "1970-01-01"
+    end
+
+    it "should colorize the string if configured to do so" do
+      @format.colors = { "birthday" => "blue" }
+      @color.should_receive(:colorize).and_return("birthday in blue")
+      @format.process(@contact).should eq "birthday in blue"
     end
 
   end
