196060514836bc2cc051ab1bf5c90e50656ac972
Henry Smith <henry@henrysmith.org>
2013-03-12 08:22:52 +0000

    Add colorization support to Ppl::Format::Contact::Age

--- lib/ppl/format/contact/age.rb
+++ lib/ppl/format/contact/age.rb
@@ -1,8 +1,20 @@
 
 class Ppl::Format::Contact::Age < Ppl::Format::Contact
 
+  attr_writer :color_adapter
+  attr_writer :colors
+
+  def initialize(colors={})
+    @colors = colors
+    @color_adapter = Ppl::Adapter::Color::Colored.new
+  end
+
   def process(contact)
-    contact.age(Date.today).to_s
+    age = contact.age(Date.today).to_s
+    if @colors["age"]
+      age = @color_adapter.colorize(age, @colors["age"])
+    end
+    age
   end
 
 end
