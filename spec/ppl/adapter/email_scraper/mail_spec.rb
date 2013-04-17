428be8de1a3662efb0abe9752422a09d1a70e06f
Henry Catalini Smith <henry@catalinismith.se>
2013-04-17 18:12:06 +0100

    Make the email scraper adapter return an array

--- spec/ppl/adapter/email_scraper/mail_spec.rb
+++ spec/ppl/adapter/email_scraper/mail_spec.rb
@@ -6,6 +6,11 @@ describe Ppl::Adapter::EmailScraper::Mail do
   end
 
   describe "#scrape_contacts" do
+
+    it "should return an array" do
+      @adapter.scrape_contacts("").should eq []
+    end
+
   end
 
 end
