5c61759e091e86f2445c7c46a6d42b22ee7bad34
Henry Catalini Smith <henry@catalinismith.se>
2013-04-17 18:32:15 +0100

    Move the existing scraping code into place in the email_scraper adapter

--- lib/ppl/adapter/email_scraper/mail.rb
+++ lib/ppl/adapter/email_scraper/mail.rb
@@ -1,8 +1,21 @@
 
+require "mail"
+
 class Ppl::Adapter::EmailScraper::Mail
 
   def scrape_contacts(email)
-    []
+    email = Mail.new(email)
+    contacts = []
+
+    from = email[:from]
+    unless from.nil?
+      sender = Ppl::Entity::Contact.new
+      sender.name = from.tree.addresses.first.display_name
+      sender.email_addresses << from.tree.addresses.first.address
+      contacts << sender
+    end
+
+    contacts
   end
 
 end
--- spec/ppl/adapter/email_scraper/mail_spec.rb
+++ spec/ppl/adapter/email_scraper/mail_spec.rb
@@ -11,6 +11,24 @@ describe Ppl::Adapter::EmailScraper::Mail do
       @adapter.scrape_contacts("").should eq []
     end
 
+    it "should scrape a contact's name and email address" do
+      email = [
+        "Date: Fri, 30 Nov 2012 17:09:33 +0000",
+        "From: Test User <test@example.org>",
+        "Message-ID: <qwertyuioasdfghjk@mail.example.org>",
+        "Subject: Test Email",
+        "To: henry@henrysmith.org",
+        "",
+        "Hey,",
+        "This is a test email.",
+        "Bye!",
+      ].join("\n")
+
+      contacts = @adapter.scrape_contacts(email)
+      contacts.first.name.should eq "Test User"
+      contacts.first.email_addresses.first.should eq "test@example.org"
+    end
+
   end
 
 end
