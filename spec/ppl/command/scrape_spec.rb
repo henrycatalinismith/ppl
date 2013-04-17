4165f0c781b6b837f041774459e429364044695e
Henry Smith <henry@henrysmith.org>
2013-04-17 19:07:00 +0100

    Read stdin the Ruby way for email scraping

--- spec/ppl/command/scrape_spec.rb
+++ spec/ppl/command/scrape_spec.rb
@@ -5,9 +5,11 @@ describe Ppl::Command::Scrape do
     @command = Ppl::Command::Scrape.new
     @input   = Ppl::Application::Input.new
     @output  = double(Ppl::Application::Output)
+    @email_scraper = double(Ppl::Adapter::EmailScraper)
     @storage = double(Ppl::Adapter::Storage)
     @address_book = double(Ppl::Entity::AddressBook)
-    @command.storage  = @storage
+    @command.storage = @storage
+    @command.email_scraper = @email_scraper
   end
 
   describe "#name" do
@@ -18,27 +20,8 @@ describe Ppl::Command::Scrape do
 
   describe "#execute" do
 
-    it "should scrape the sender's address if requested" do
-      @input.options[:sender] = true
-      @input.stdin = [
-        "Received: by 10.58.205.235 with HTTP; Fri, 30 Nov 2012 09:09:33 -0800 (PST)",
-        "Date: Fri, 30 Nov 2012 17:09:33 +0000",
-        "From: Test User <test@example.org>",
-        "Delivered-To: henry@henrysmith.org",
-        "Message-ID: <qwertyuioasdfghjk@mail.gmail.com>",
-        "Subject: Test Email",
-        "To: henry@henrysmith.org",
-        "",
-        "Hey,",
-        "This is a test email.",
-        "Bye!",
-      ].join("\n")
-
-      @storage.should_receive(:save_contact) do |contact|
-        contact.name.should eq "Test User"
-        contact.email_addresses.first.should eq "test@example.org"
-      end
-
+    it "should read input from ARGF" do
+      @input.argf.should_receive(:read)
       @command.execute(@input, @output)
     end
 
