9e250b1bc5c9d69dba70ad8e49f5b3870673052f
Henry Catalini Smith <henry@catalinismith.com>
2013-04-17 18:33:54 +0100

    Refactor the email sender scraping into its own method

--- lib/ppl/adapter/email_scraper/mail.rb
+++ lib/ppl/adapter/email_scraper/mail.rb
@@ -7,15 +7,24 @@ class Ppl::Adapter::EmailScraper::Mail
     email = Mail.new(email)
     contacts = []
 
+    sender = scrape_sender(email)
+    unless sender.nil?
+      contacts << sender
+    end
+
+    contacts
+  end
+
+  private
+
+  def scrape_sender(email)
     from = email[:from]
     unless from.nil?
       sender = Ppl::Entity::Contact.new
       sender.name = from.tree.addresses.first.display_name
       sender.email_addresses << from.tree.addresses.first.address
-      contacts << sender
+      sender
     end
-
-    contacts
   end
 
 end
