b0682268bb994852f9474b3bf9547884d4ecc365
Henry Smith <henry@henrysmith.org>
2013-04-17 13:07:18 +0100

    Implement the simplest possible form of a "scrape" command for h2s/ppl#23
    This isn't done yet. It's not been tested thoroughly enough to release, and it
    still has a few bits of missing functionality.
    
    - It needs to autogenerate a contact ID based on their display_name
    - It needs to not trip over and die if any of the properties it depends on
      aren't present
    
    I'm thinking this might actually be another prime candidate for a Ppl::Adapter
    mail-scraping interface.

--- spec/ppl/command/scrape_spec.rb
+++ spec/ppl/command/scrape_spec.rb
@@ -17,6 +17,31 @@ describe Ppl::Command::Scrape do
   end
 
   describe "#execute" do
+
+    it "should scrape the sender's address if requested" do
+      @input.options[:sender] = true
+      @input.stdin = [
+        "Received: by 10.58.205.235 with HTTP; Fri, 30 Nov 2012 09:09:33 -0800 (PST)",
+        "Date: Fri, 30 Nov 2012 17:09:33 +0000",
+        "From: Test User <test@example.org>",
+        "Delivered-To: henry@henrysmith.org",
+        "Message-ID: <qwertyuioasdfghjk@mail.gmail.com>",
+        "Subject: Test Email",
+        "To: henry@henrysmith.org",
+        "",
+        "Hey,",
+        "This is a test email.",
+        "Bye!",
+      ].join("\n")
+
+      @storage.should_receive(:save_contact) do |contact|
+        contact.name.should eq "Test User"
+        contact.email_addresses.first.should eq "test@example.org"
+      end
+
+      @command.execute(@input, @output)
+    end
+
   end
 
 end
