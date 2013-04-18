b68f575132a44c27989dd6d33e9ea1bd3731eced
Henry Catalini Smith <henry@catalinismith.com>
2013-04-18 08:28:18 +0100

    Only save the contact if the user approves

--- spec/ppl/command/scrape_spec.rb
+++ spec/ppl/command/scrape_spec.rb
@@ -4,6 +4,7 @@ describe Ppl::Command::Scrape do
   before(:each) do
     @command = Ppl::Command::Scrape.new
     @input   = Ppl::Application::Input.new
+    @input.stdin = double(IO)
     @output  = double(Ppl::Application::Output)
     @email_scraper = double(Ppl::Adapter::EmailScraper)
     @storage = double(Ppl::Adapter::Storage)
@@ -23,6 +24,9 @@ describe Ppl::Command::Scrape do
     before(:each) do
       @input.argf.stub(:read)
       @email_scraper.stub(:scrape_contacts).and_return([])
+      @input.stdin.stub(:reopen)
+      Readline.stub(:readline)
+      @storage.stub(:save_contact)
     end
 
     it "should read input from ARGF" do
@@ -36,7 +40,16 @@ describe Ppl::Command::Scrape do
     end
 
     it "should save all contacts returned by the scraper" do
+      @input.options[:quiet] = true
       @email_scraper.stub(:scrape_contacts).and_return([1, 2, 3])
+      @storage.should_receive(:save_contact).exactly(3).times
+      @command.execute(@input, @output)
+    end
+
+    it "should save the contact if the user approves" do
+      @email_scraper.stub(:scrape_contacts).and_return([1, 2, 3])
+      @input.stdin.should_receive(:reopen)
+      Readline.should_receive(:readline).exactly(3).times.and_return("y")
       @storage.should_receive(:save_contact).exactly(3).times
       @command.execute(@input, @output)
     end
