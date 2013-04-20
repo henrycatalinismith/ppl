e7794f5ca0a40215331757b790d4cd3fac76d272
Henry Smith <henry@henrysmith.org>
2013-04-20 10:55:51 +0100

    Amend the phone command to deal with the --type option

--- spec/ppl/command/phone_spec.rb
+++ spec/ppl/command/phone_spec.rb
@@ -11,5 +11,41 @@ describe Ppl::Command::Phone do
     end
   end
 
+  describe "#execute" do
+
+    before(:each) do
+      @contact = Ppl::Entity::Contact.new
+      @storage = double(Ppl::Adapter::Storage)
+      @input = Ppl::Application::Input.new
+      @input.arguments = ["jdoe", "01234567"]
+      @storage.stub(:require_contact).and_return(@contact)
+      @storage.stub(:save_contact)
+      @command.storage = @storage
+    end
+
+    it "should save phone numbers as instances of Ppl::Entity::PhoneNumber" do
+      @storage.should_receive(:save_contact) do |c|
+        c.phone_numbers.first.should be_a(Ppl::Entity::PhoneNumber)
+      end
+      @command.execute(@input, @output)
+    end
+
+    it "should save the given number as an attribute of the PhoneNumber" do
+      @storage.should_receive(:save_contact) do |c|
+        c.phone_numbers.first.number.should eq "01234567"
+      end
+      @command.execute(@input, @output)
+    end
+
+    it "should save the given type alongside the number" do
+      @input.options[:type] = "cell"
+      @storage.should_receive(:save_contact) do |c|
+        c.phone_numbers.first.type.should eq "cell"
+      end
+      @command.execute(@input, @output)
+    end
+
+  end
+
 end
 
