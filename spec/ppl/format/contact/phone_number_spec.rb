32e2291b45cc2691172ad27eabb966ffb3b71d38
Henry Catalini Smith <henry@catalinismith.se>
2013-04-20 11:03:48 +0100

    Modify the phone number output formatter to include types

--- spec/ppl/format/contact/phone_number_spec.rb
+++ spec/ppl/format/contact/phone_number_spec.rb
@@ -4,8 +4,10 @@ describe Ppl::Format::Contact::PhoneNumber do
   before(:each) do
     @format  = Ppl::Format::Contact::PhoneNumber.new
     @contact = Ppl::Entity::Contact.new
+    @number  = Ppl::Entity::PhoneNumber.new("0123456789")
     @color   = double(Ppl::Adapter::Color)
     @format.color_adapter = @color
+    @contact.phone_numbers << @number
   end
 
   describe "#process" do
@@ -15,8 +17,12 @@ describe Ppl::Format::Contact::PhoneNumber do
     end
 
     it "should return the contact's phone number if it is set" do
-      @contact.phone_numbers.push("0123456789")
       @format.process(@contact).should eq "0123456789"
+    end
+
+    it "should include the type of phone number if it's sset" do
+      @number.type = "cell"
+      @format.process(@contact).should eq "0123456789 (cell)"
     end
 
     it "should colorize the string if configured to do so" do
