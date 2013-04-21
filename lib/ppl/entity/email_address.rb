4d7bab8842a72056b021b499cfca6301767b9482
Henry Catalini Smith <henry@catalinismith.com>
2013-04-21 18:03:23 +0100

    Add an EmailAddress entity

---
+++ lib/ppl/entity/email_address.rb
@@ -0,0 +1,11 @@
+
+class Ppl::Entity::EmailAddress
+
+  attr_accessor :address
+
+  def initialize(address = nil)
+    @address = address
+  end
+
+end
+
---
+++ spec/ppl/entity/email_address_spec.rb
@@ -0,0 +1,22 @@
+
+describe Ppl::Entity::EmailAddress do
+
+  before(:each) do
+    @email_address = Ppl::Entity::EmailAddress.new
+  end
+
+  describe "#address" do
+    it "should return a value" do
+      @email_address.address.should eq nil
+    end
+  end
+
+  describe "#initialize" do
+    it "should accept an address" do
+      email = Ppl::Entity::EmailAddress.new("bob@example.org")
+      email.address.should eq "bob@example.org"
+    end
+  end
+
+end
+
