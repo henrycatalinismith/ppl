6bfcc8e884cc0b8c1c89f254cd7ce14e6ee80ae2
Henry Smith <henry@henrysmith.org>
2013-04-21 21:44:33 +0100

    Add an EmailAddress service

---
+++ lib/ppl/service/email_address.rb
@@ -0,0 +1,20 @@
+
+class Ppl::Service::EmailAddress
+
+  attr_writer :storage
+
+  def add(contact, address)
+    email_address = Ppl::Entity::EmailAddress.new(address)
+    contact.email_addresses << email_address
+    @storage.save_contact(contact)
+  end
+
+  def remove(contact, address)
+    contact.email_addresses.select! do |email_address|
+      email_address.address != address
+    end
+    @storage.save_contact(contact)
+  end
+
+end
+
