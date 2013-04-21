45b1ca90b57126d2bdf691aefc0679a87bb603c2
Henry Catalini Smith <henry@catalinismith.com>
2013-04-21 19:15:46 +0100

    Fix ppl email to work with the new EmailAddress entity

--- lib/ppl/command/email.rb
+++ lib/ppl/command/email.rb
@@ -15,5 +15,35 @@ class Ppl::Command::Email < Ppl::Command::Attribute
     end
   end
 
+  def add_attribute(input, output)
+    contact = @storage.require_contact(input.arguments.shift)
+    if new_email_address?(contact, input.arguments[0])
+      add_new_email_address(contact, input)
+    end
+    @storage.save_contact(contact)
+    true
+  end
+
+  def remove_attribute(input, output)
+    contact = @storage.require_contact(input.arguments[0])
+    contact.email_addresses.select! { |ea| ea.address != input.arguments[1] }
+    @storage.save_contact(contact)
+  end
+
+  private
+
+  def new_email_address?(contact, input_address)
+    matching_addresses = contact.email_addresses.select do |em|
+      em.address == input_address
+    end
+    matching_addresses.length < 1
+  end
+
+  def add_new_email_address(contact, input)
+    email_address = Ppl::Entity::EmailAddress.new
+    email_address.address = input.arguments[0]
+    contact.email_addresses << email_address
+  end
+
 end
 
