62de7246777d652b1224ec41af150e7103eb799b
Henry Catalini Smith <henry@catalinismith.se>
2013-04-22 18:48:07 +0100

    Business logic for preferred email addresses

--- lib/ppl/service/email_address.rb
+++ lib/ppl/service/email_address.rb
@@ -10,6 +10,12 @@ class Ppl::Service::EmailAddress
   end
 
   def update(contact, address, options)
+    email_address = contact.email_addresses.find { |e| e.address == address }
+    if options[:preferred] == true
+      contact.email_addresses.each { |e| e.preferred = (e.address == address) }
+    elsif options[:preferred] == false
+      email_address.preferred = false
+    end
     @storage.save_contact(contact)
   end
 
