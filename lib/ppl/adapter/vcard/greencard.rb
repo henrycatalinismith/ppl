c1815c7a4b7b440a24594565ec3db621e35cfae4
Henry Catalini Smith <henry@catalinismith.com>
2013-04-22 18:20:17 +0100

    Add vCard encoding support for Ppl::Entity::EmailAddress#preferred

--- lib/ppl/adapter/vcard/greencard.rb
+++ lib/ppl/adapter/vcard/greencard.rb
@@ -58,7 +58,9 @@ class Ppl::Adapter::Vcard::GreenCard
 
   def encode_email_addresses(contact, vcard_maker)
     contact.email_addresses.each do |email_address|
-      vcard_maker.add_email(email_address.address)
+      vcard_maker.add_email(email_address.address) do |vcard_email|
+        vcard_email.preferred = email_address.preferred
+      end
     end
   end
 
