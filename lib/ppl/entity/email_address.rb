b98fc3326e8fa1daf32ff448029943c62fcdc357
Henry Smith <henry@henrysmith.org>
2013-04-22 13:20:42 +0100

    Add "preferred" attribute to Ppl::Entity::EmailAddress

--- lib/ppl/entity/email_address.rb
+++ lib/ppl/entity/email_address.rb
@@ -2,9 +2,11 @@
 class Ppl::Entity::EmailAddress
 
   attr_accessor :address
+  attr_accessor :preferred
 
   def initialize(address = nil)
     @address = address
+    @preferred = false
   end
 
 end
