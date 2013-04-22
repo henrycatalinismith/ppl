fd3b4e763a3596a6c20d223d2020108ee0b7630b
Henry Catalini Smith <henry@catalinismith.com>
2013-04-22 22:39:34 +0100

    Rebuild ppl show <contact>

--- lib/ppl/command/phone.rb
+++ lib/ppl/command/phone.rb
@@ -27,12 +27,21 @@ class Ppl::Command::Phone < Ppl::Application::Command
   private
 
   def determine_action(input)
-    :list_address_book_phone_numbers
+    if input.arguments.length < 1
+      :list_address_book_phone_numbers
+    elsif input.arguments.length < 2
+      :show_contact_phone_numbers
+    end
   end
 
   def list_address_book_phone_numbers(input, output)
     address_book = @storage.load_address_book
     output.line(@list_format.process(address_book))
+  end
+
+  def show_contact_phone_numbers(input, output)
+    contact = @storage.require_contact(input.arguments[0])
+    output.line(@show_format.process(contact))
   end
 
 end
