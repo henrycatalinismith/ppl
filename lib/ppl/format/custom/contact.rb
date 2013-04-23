18ff906753871942f53c0d36b7ba3d465b1488b0
Henry Catalini Smith <henry@catalinismith.com>
2013-04-23 21:40:13 +0100

    Implement the first few bits of custom contact formatting

--- lib/ppl/format/custom/contact.rb
+++ lib/ppl/format/custom/contact.rb
@@ -5,5 +5,11 @@ class Ppl::Format::Custom::Contact < Ppl::Format::Custom
     contact.name
   end
 
+  format :b do |contact|
+    unless contact.birthday.nil?
+      contact.birthday.strftime("%Y-%m-%d")
+    end
+  end
+
 end
 
