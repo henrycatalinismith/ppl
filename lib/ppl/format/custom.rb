f738cbfe0085ebcb3e7707567f9b6bb8fd26f461
Henry Catalini Smith <henry@catalinismith.se>
2013-04-23 21:33:22 +0100

    Implement Ppl::Format::Custom
    This is based on some really neat ideas gleaned from
    https://github.com/mark/string_formatter
    
    Thanks Mark!

--- lib/ppl/format/custom.rb
+++ lib/ppl/format/custom.rb
@@ -1,7 +1,42 @@
 
+
 class Ppl::Format::Custom
 
   attr_accessor :format
+
+  def self.format(symbol, &block)
+    @format_blocks ||= {}
+    @format_blocks[symbol] = block
+  end
+
+  def self.process(key, object)
+    if @format_blocks[key]
+      @format_blocks[key][object]
+    else
+      key
+    end
+  end
+
+  def initialize(format)
+    @format = format
+  end
+
+  def process(object)
+    @object = object
+    string_pieces = @format.scan(/[^%]+|%./)
+    string_pieces.map(&method(:process_piece)).join
+  end
+
+  private
+
+  def process_piece(string)
+    if string[0...1] == "%"
+      letter = string[1..-1]
+      self.class.process(letter.to_sym, @object)
+    else
+      string
+    end
+  end
 
 end
 
