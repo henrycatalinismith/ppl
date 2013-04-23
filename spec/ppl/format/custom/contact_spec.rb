9b79eb17b5178bf81b2b424730dabd5a48a8c455
Henry Catalini Smith <henry@catalinismith.com>
2013-04-23 21:33:22 +0100

    Implement Ppl::Format::Custom
    This is based on some really neat ideas gleaned from
    https://github.com/mark/string_formatter
    
    Thanks Mark!

--- spec/ppl/format/custom/contact_spec.rb
+++ spec/ppl/format/custom/contact_spec.rb
@@ -1,12 +1,6 @@
 
 describe Ppl::Format::Custom::Contact do
 
-  before(:each) do
-    @format = Ppl::Format::Custom::Contact.new
-    @contact = Ppl::Entity::Contact.new
-    @contact.name = "John Smith"
-  end
-
 end
 
 
