e891903b04b3401b7392c36da0f981a5d3ee3a51
Henry Catalini Smith <henry@catalinismith.se>
2013-04-23 22:10:25 +0100

    Process ppl ls output with a custom format if requested

--- spec/ppl/command/ls_spec.rb
+++ spec/ppl/command/ls_spec.rb
@@ -7,10 +7,12 @@ describe Ppl::Command::Ls do
     @output  = double(Ppl::Application::Output)
     @storage = double(Ppl::Adapter::Storage)
     @format  = double(Ppl::Format::AddressBook)
+    @custom  = double(Ppl::Format::Custom)
     @address_book = double(Ppl::Entity::AddressBook)
 
-    @command.format   = @format
-    @command.storage  = @storage
+    @command.custom_format  = @custom
+    @command.default_format = @format
+    @command.storage        = @storage
   end
 
   describe "#name" do
@@ -20,12 +22,24 @@ describe Ppl::Command::Ls do
   end
 
   describe "#execute" do
+
     it "should show the list of contacts in the address book" do
       @storage.should_receive(:load_address_book).and_return(@address_book)
       @format.should_receive(:process).and_return("list of contacts")
       @output.should_receive(:line).with("list of contacts")
       @command.execute(@input, @output)
     end
+
+    it "should let the user specify a custom format" do
+      @input.options[:format] = "%n"
+      @storage.should_receive(:load_address_book).and_return(@address_book)
+      @address_book.should_receive(:contacts).and_return([1])
+      @custom.should_receive(:format=).with("%n")
+      @custom.should_receive(:process).and_return("list of contacts")
+      @output.should_receive(:line).with("list of contacts")
+      @command.execute(@input, @output)
+    end
+
   end
 
 end
