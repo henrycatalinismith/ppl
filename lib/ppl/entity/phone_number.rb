5498fecda86c85a1395fd1fd9d1fec6617243894
Henry Smith <henry@henrysmith.org>
2013-04-23 08:16:02 +0100

    Make the phone number service mark numbers as preferred

--- lib/ppl/entity/phone_number.rb
+++ lib/ppl/entity/phone_number.rb
@@ -3,9 +3,11 @@ class Ppl::Entity::PhoneNumber
 
   attr_accessor :number
   attr_accessor :type
+  attr_accessor :preferred
 
   def initialize(number = nil, type = nil)
     @number = number
+    @preferred = false
     @type = type
   end
 
