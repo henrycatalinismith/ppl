9afd9ff17c3389b7517c6f0e89135e848dd48879
Henry Smith <henry@henrysmith.org>
2013-04-24 07:41:43 +0100

    Add a phone number custom formatter

---
+++ lib/ppl/format/custom/phone_number.rb
@@ -0,0 +1,19 @@
+
+class Ppl::Format::Custom::PhoneNumber < Ppl::Format::Custom
+
+  format :n do |phone_number|
+    phone_number.number
+  end
+
+  format :t do |phone_number|
+    phone_number.type
+  end
+
+  format :f do |phone_number|
+    if phone_number.preferred
+      "*"
+    end
+  end
+
+end
+
