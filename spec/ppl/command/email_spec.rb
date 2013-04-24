537499febaa7caa4a229ab496d8e511fa4f8a8c5
Henry Catalini Smith <henry@catalinismith.se>
2013-04-24 13:34:15 +0100

    Add custom output formatting to ppl email

--- spec/ppl/command/email_spec.rb
+++ spec/ppl/command/email_spec.rb
@@ -21,12 +21,14 @@ describe Ppl::Command::Email do
       @output = double(Ppl::Application::Output)
       @list_format = double(Ppl::Format::AddressBook)
       @show_format = double(Ppl::Format::Contact)
+      @custom_format = double(Ppl::Format::Custom)
       @storage.stub(:require_contact).and_return(@contact)
       @storage.stub(:save_contact)
       @command.storage = @storage
       @command.email_service = @service
       @command.list_format = @list_format
       @command.show_format = @show_format
+      @command.custom_format = @custom_format
     end
 
     it "should list all email addresses by default" do
@@ -41,6 +43,17 @@ describe Ppl::Command::Email do
       @storage.should_receive(:require_contact).and_return(@contact)
       @show_format.should_receive(:process).and_return("imagine this is a list")
       @output.should_receive(:line).with("imagine this is a list")
+      @command.execute(@input, @output)
+    end
+
+    it "should output addresses in a custom format if one is given" do
+      @input.arguments << "jdoe"
+      @input.options[:format] = "%e"
+      @contact.email_addresses << Ppl::Entity::EmailAddress.new("jdoe@example.org")
+      @storage.should_receive(:require_contact).and_return(@contact)
+      @custom_format.should_receive(:format=).with("%e")
+      @custom_format.should_receive(:process).and_return("")
+      @output.should_receive(:line).with("")
       @command.execute(@input, @output)
     end
     
