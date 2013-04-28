6e6f9bce4ac940f06dc6dc773179275d2d68057b
Henry Catalini Smith <henry@catalinismith.com>
2013-04-28 13:18:48 +0100

    Revert "Install coveralls"
    This reverts commit c64eba2ef563c33c2e862ec0cfdf1c008bd01eb3.
    
    Coveralls depends on the "colorize" gem:
    https://github.com/lemurheavy/coveralls-ruby/blob/2081d0382b7bd8796634d39d1c06f65241dac836/coveralls-ruby.gemspec#L19
    
    This gem's monkey patches of Ruby's String class override the colored gem's
    monkey patches and break the build.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,5 @@ require "pathname"
 bin_file = Pathname.new(__FILE__).realpath
 $:.unshift File.expand_path("../../lib", bin_file)
 
-if ENV['CI']
-  require "coveralls"
-  Coveralls.wear!
-end
-
 require "ppl"
 
