6c0701e7bcb4086d08dc7023821766692ed87b81
Henry Smith <henry@henrysmith.org>
2013-05-04 10:11:56 +0100

    Create Ppl::Service::PostalAddress#update

--- spec/ppl/service/postal_address_spec.rb
+++ spec/ppl/service/postal_address_spec.rb
@@ -4,6 +4,7 @@ describe Ppl::Service::PostalAddress do
   before(:each) do
     @service = Ppl::Service::PostalAddress.new
     @contact = Ppl::Entity::Contact.new
+    @address = Ppl::Entity::PostalAddress.new
   end
 
   describe "#add" do
@@ -42,6 +43,45 @@ describe Ppl::Service::PostalAddress do
     it "sets the region of the new address" do
       @address.region.should eq "South West"
     end
+  end
+
+
+  describe "#update" do
+
+    before(:each) do
+      @contact.postal_addresses << @address
+      @service.update(@contact, @address, {
+        :country     => "United Kingdom",
+        :locality    => "Bristol",
+        :street      => "1 Broad Mead",
+        :po_box      => "550011",
+        :postal_code => "BS1 1SB",
+        :region      => "South West",
+      })
+    end
+
+    it "doesn't change the number of addresses owned by the contact" do
+      @contact.postal_addresses.length.should eq 1
+    end
+    it "sets the country of the new address" do
+      @address.country.should eq "United Kingdom"
+    end
+    it "sets the locality of the new address" do
+      @address.locality.should eq "Bristol"
+    end
+    it "sets the street of the new address" do
+      @address.street.should eq "1 Broad Mead"
+    end
+    it "sets the PO box of the new address" do
+      @address.po_box.should eq "550011"
+    end
+    it "sets the postal code of the new address" do
+      @address.postal_code.should eq "BS1 1SB"
+    end
+    it "sets the region of the new address" do
+      @address.region.should eq "South West"
+    end
+
   end
 
 end
