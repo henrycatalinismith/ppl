c0fb1b2c36588ee8c5485030ce98e1a0c454cce2
Henry Catalini Smith <henry@catalinismith.com>
2013-05-07 18:34:08 +0100

    Implement ppl post single postal address display

--- lib/ppl/command/post.rb
+++ lib/ppl/command/post.rb
@@ -7,6 +7,7 @@ class Ppl::Command::Post < Ppl::Application::Command
   attr_writer :address_service
   attr_writer :contact_format
   attr_writer :address_book_format
+  attr_writer :postal_address_format
 
   def options(parser, options)
     parser.banner = "usage: ppl post <contact> [address]"
@@ -42,21 +43,31 @@ class Ppl::Command::Post < Ppl::Application::Command
 
   def determine_action(input)
     if input.arguments[0].nil?
-      :list_postal_addresses
+      :show_address_book
     elsif input.options.empty?
-      :show_postal_address
+      :show_contact
     else
-      :set_postal_address
+      :show_postal_address
     end
   end
 
-  def list_postal_addresses(input, output)
+  def determine_action(input)
+    if input.arguments.length < 1
+      :list_address_book_postal_addresses
+    elsif input.arguments.length < 2
+      :show_contact_postal_addresses
+    else
+      :show_postal_address
+    end
+  end
+
+  def list_address_book_postal_addresses(input, output)
     address_book = @storage.load_address_book
     address_list = @address_book_format.process(address_book)
     output.line(address_list)
   end
 
-  def show_postal_address(input, output)
+  def show_contact_postal_addresses(input, output)
     contact = @storage.require_contact(input.arguments[0])
     address = @contact_format.process(contact)
     if address != ""
@@ -65,6 +76,14 @@ class Ppl::Command::Post < Ppl::Application::Command
     else
       false
     end
+  end
+
+  def show_postal_address(input, output)
+    contact = @storage.require_contact(input.arguments[0])
+    address = contact.postal_addresses.find { |pa| pa.id = input.arguments[1] }
+    display = @postal_address_format.process(address)
+    output.line(display)
+    true
   end
 
 end
