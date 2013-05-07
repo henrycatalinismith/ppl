9b51ab454d351256018b25a2ea358ec9f276a131
Henry Catalini Smith <henry@catalinismith.se>
2013-05-07 21:37:42 +0100

    Update the postal address service API to match that used by ppl post

--- lib/ppl/service/postal_address.rb
+++ lib/ppl/service/postal_address.rb
@@ -1,14 +1,16 @@
 
 class Ppl::Service::PostalAddress
 
-  def add(contact, options)
+  def add(contact, address_id, options)
     address = Ppl::Entity::PostalAddress.new
+    address.id = address_id
     update_postal_address(address, options)
     contact.postal_addresses << address
   end
 
   def update(contact, address_id, options)
-    update_postal_address(address_id, options)
+    address = contact.postal_addresses.find { |p| p.id == address_id }
+    update_postal_address(address, options)
   end
 
   def remove(contact, address_id)
