ac8b6622bdd95e22e5cd942dee6547842bae412d
Henry Smith <henry@henrysmith.org>
2013-05-08 14:41:25 +0100

    Mark preferred addresses with a star in output

--- lib/ppl/format/postal_address/one_line.rb
+++ lib/ppl/format/postal_address/one_line.rb
@@ -3,13 +3,17 @@ class Ppl::Format::PostalAddress::OneLine < Ppl::Format::AddressBook
 
   def process(postal_address, table)
     table.add_row({
-      :star         => " ",
+      :star         => format_star(postal_address),
       :address_id   => postal_address.id,
       :address_text => format_address_text(postal_address),
     })
   end
 
   private
+
+  def format_star(postal_address)
+    postal_address.preferred ? "*" : " "
+  end
 
   def format_address_text(postal_address)
     [
