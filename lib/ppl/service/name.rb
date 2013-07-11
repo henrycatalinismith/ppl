d8b1f1a1b1383b93d272f8dae8ad86191d6da437
Henry Catalini Smith <henry@catalinismith.com>
2013-07-11 12:45:49 +0100

    Have a bit of a guess at contacts' names' structures
    This follows some ideas proposed in h2s/ppl#42

--- lib/ppl/service/name.rb
+++ lib/ppl/service/name.rb
@@ -2,8 +2,11 @@
 class Ppl::Service::Name
 
   def parse(full_name)
+    parts = full_name.split " "
     name = Ppl::Entity::Name.new
     name.full = full_name
+    name.given = parts.shift unless parts[0].nil?
+    name.family = parts.join(" ") unless parts[0].nil?
     name
   end
 
--- spec/ppl/service/name_spec.rb
+++ spec/ppl/service/name_spec.rb
@@ -11,6 +11,14 @@ describe Ppl::Service::Name do
       @service.parse("John Smith").full.should eq "John Smith"
     end
 
+    it "uses the first word for the given name" do
+      @service.parse("John Smith").given.should eq "John"
+    end
+
+    it "uses the rest of the string for the family names" do
+      @service.parse("John Fitzgerald Donald Smith").family.should eq "Fitzgerald Donald Smith"
+    end
+
   end
 
 end
