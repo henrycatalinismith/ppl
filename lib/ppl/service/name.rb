56f33592168dedba80558253ea99a89f22c41f9e
Henry Catalini Smith <henry@catalinismith.se>
2013-07-13 16:41:25 +0100

    Implement Ppl::Service::Name#update

--- lib/ppl/service/name.rb
+++ lib/ppl/service/name.rb
@@ -10,5 +10,14 @@ class Ppl::Service::Name
     name
   end
 
+  def update(name, new_values)
+    [:full, :given, :family, :middle, :prefix, :suffix].each do |property_name|
+      if !new_values[property_name].nil?
+        setter = (property_name.to_s + "=").to_sym
+        name.send(setter, new_values[property_name])
+      end
+    end
+  end
+
 end
 
--- spec/ppl/service/name_spec.rb
+++ spec/ppl/service/name_spec.rb
@@ -3,6 +3,7 @@ describe Ppl::Service::Name do
 
   before(:each) do
     @service = Ppl::Service::Name.new
+    @name = Ppl::Entity::Name.new
   end
 
   describe "#parse" do
@@ -17,6 +18,40 @@ describe Ppl::Service::Name do
 
     it "uses the rest of the string for the family names" do
       @service.parse("John Fitzgerald Donald Smith").family.should eq "Fitzgerald Donald Smith"
+    end
+
+  end
+
+  describe "#update" do
+
+    it "updates the full name" do
+      @service.update(@name, {:full => "Abc Def"})
+      @name.full.should eq "Abc Def"
+    end
+
+    it "updates the given names" do
+      @service.update(@name, {:given => "John"})
+      @name.given.should eq "John"
+    end
+
+    it "updates the family names" do
+      @service.update(@name, {:family => "Smith"})
+      @name.family.should eq "Smith"
+    end
+
+    it "updates the middle names" do
+      @service.update(@name, {:middle => "Arnold"})
+      @name.middle.should eq "Arnold"
+    end
+
+    it "updates the name prefix" do
+      @service.update(@name, {:prefix => "Mr"})
+      @name.prefix.should eq "Mr"
+    end
+
+    it "updates the name suffix" do
+      @service.update(@name, {:suffix => "BSc"})
+      @name.suffix.should eq "BSc"
     end
 
   end
