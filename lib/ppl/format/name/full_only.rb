c793e710d888be0217ceec5532b0e086ad3174c2
Henry Smith <henry@henrysmith.org>
2013-07-13 16:54:06 +0100

    Port existing name colorization code to Ppl::Format::Name::FullOnly

--- lib/ppl/format/name/full_only.rb
+++ lib/ppl/format/name/full_only.rb
@@ -1,8 +1,30 @@
 
 class Ppl::Format::Name::FullOnly < Ppl::Format::Name
 
+  attr_writer :color_adapter
+  attr_writer :colors
+
+  def initialize(colors={})
+    @colors = colors
+    @color_adapter = Ppl::Adapter::Color::Colored.new
+  end
+
   def process(name)
-    name.full
+    output = ""
+    if !name.full.nil?
+      output += name.full
+    end
+    colorize_output(output)
+  end
+
+  private
+
+  def colorize_output(string)
+    if @colors["name"]
+      @color_adapter.colorize(string, @colors["name"])
+    else
+      string
+    end
   end
 
 end
