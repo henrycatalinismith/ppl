5c30160401f76d59b82c8857ce80aa635241d611
Henry Catalini Smith <henry@catalinismith.com>
2013-12-30 08:31:25 +0000

    Set up Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,10 @@ require "pathname"
 bin_file = Pathname.new(__FILE__).realpath
 $:.unshift File.expand_path("../../lib", bin_file)
 
+if ENV['CI']
+  require "coveralls"
+  Coveralls.wear!
+end
+
 require "ppl"
 
