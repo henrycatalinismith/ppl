36c178ce79a3ead03af2875f1b36c37684d0e855
Henry Smith <henry@henrysmith.org>
2017-04-23 11:30:49 +0200

    Remove coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,7 @@
-
 require "fakefs/safe"
 require "pathname"
 
 bin_file = Pathname.new(__FILE__).realpath
 $:.unshift File.expand_path("../../lib", bin_file)
 
-if ENV['CI']
-  require "coveralls"
-  Coveralls.wear!
-end
-
 require "ppl"
-
