451ade99811ec174fe066f4b383aebe7db61ac5a
Henry Smith <henry@henrysmith.org>
2017-04-26 19:25:48 +0200

    Make ppl add work

--- src/repository.js
+++ src/repository.js
@@ -1,6 +1,25 @@
+const Git = require('nodegit');
+const fs = require('fs');
+
 module.exports.saveContact = contact => new Promise(resolve => {
-  setTimeout(() => {
-    console.log('');
-    resolve();
-  }, 100);
+  const repoPath = `${process.cwd()}/tmp/test`;
+  const filePath = `${repoPath}/${contact.id}.vcf`;
+
+  return Git.Repository.open(repoPath).then(repository => {
+    const author = Git.Signature.create(
+      'Test',
+      'test@example.org',
+      Date.now(),
+      (new Date).getTimezoneOffset()
+    );
+
+    fs.writeFileSync(filePath, contact.card.toString(), 'utf-8');
+
+    return repository.createCommitOnHead(
+      [`${contact.id}.vcf`],
+      author,
+      author,
+      `save_contact(${contact.id})`
+    );
+  });
 });
