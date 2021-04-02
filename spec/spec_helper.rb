05c2fb1f5a4bb1b97853a095595128f0f1c27f0d
Henry Catalini Smith <henry@catalinismith.se>
2021-04-02 20:55:40 +0200

    silence false positive warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require "fakefs/safe"
 require "pathname"
 require "pry"
+require "rspec"
 
 bin_file = Pathname.new(__FILE__).realpath
 $:.unshift File.expand_path("../../lib", bin_file)
+
+RSpec::Expectations.configuration.on_potential_false_positives = :nothing
 
 require "ppl"
