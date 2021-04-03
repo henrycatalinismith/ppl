ab824bb09d8355d0552b836b6a9cf7d8f56b3a5c
Henry Catalini Smith <henry@catalinismith.com>
2021-04-03 11:50:12 +0200

    set up scss watch for docs dev

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
+const Eleventy = require("@11ty/eleventy/src/Eleventy.js")
+const chokidar = require("chokidar")
 const fs = require("fs-extra")
 const htmlmin = require("html-minifier")
-const path = require("path")
 const sass = require("sass")
 
 function minifyHtml(html) {
@@ -9,6 +10,15 @@ function minifyHtml(html) {
     removeComments: true,
     collapseWhitespace: true
   })
+}
+
+function monkeypatch(cls, fn) {
+  const orig = cls.prototype[fn.name][`_PS_original`] || cls.prototype[fn.name]
+  function wrapped() {
+    return fn.bind(this, orig).apply(this, arguments)
+  }
+  wrapped[`_PS_original`] = orig
+  cls.prototype[fn.name] = wrapped
 }
 
 const minifyPlugin = {
@@ -26,9 +36,43 @@ const minifyPlugin = {
   }
 }
 
+function compileSass() {
+  const { css } = sass.renderSync({
+    file: "docs/style.scss",
+  })
+  fs.writeFileSync("_site/style.css", css)
+}
+
 module.exports = function(eleventyConfig) {
   console.log("ppl docs")
+
+  fs.ensureDirSync("_site")
+  fs.emptyDirSync("_site")
+
   eleventyConfig.addPlugin(minifyPlugin)
+
+  compileSass()
+
+  if (process.argv.includes("--serve")) {
+    setImmediate(function() {
+      const Eleventy = require("@11ty/eleventy/src/Eleventy.js")
+      if (Eleventy.prototype) {
+        function watch(original) {
+          const watcher = chokidar.watch(["./docs/style.scss"], {
+            persistent: true
+          })
+          const compileAndReload = eleventyInstance => () => {
+            compileSass()
+            this.eleventyServe.reload()
+          }
+          watcher.on("add", compileAndReload(this))
+          watcher.on("change", compileAndReload(this))
+          return original.apply(this)
+        }
+        monkeypatch(Eleventy, watch)
+      }
+    })
+  }
 
   const input = "docs"
 
